85b271356cf352be7b923639e8ca1b6a8bb44445
KimChanJin97 <cjkimhello97@gmail.com>
2024-03-14 16:57:17 +0900

    chore: gradle 및 gitignore 추가

---
+++ src/main/java/capstone/facefriend/FacefriendApplication.java
@@ -0,0 +1,13 @@
+package capstone.facefriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacefriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacefriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/facefriend/FacefriendApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.facefriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacefriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
