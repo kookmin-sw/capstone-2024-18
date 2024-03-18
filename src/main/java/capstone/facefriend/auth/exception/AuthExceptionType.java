53939426f7cb6672a0909759248e050db512f343
KimChanJin97 <cjkimhello97@gmail.com>
2024-03-19 00:39:59 +0900

    fix: exception.status() null값 나오는 에러 수정

--- src/main/java/capstone/facefriend/auth/exception/AuthExceptionType.java
+++ src/main/java/capstone/facefriend/auth/exception/AuthExceptionType.java
@@ -8,7 +8,7 @@ public enum AuthExceptionType implements ExceptionType {
     INVALID_AUTH_PROVIDER(Status.UNAUTHORIZED, 2001, "지원하지 않는 로그인 플랫폼입니다."),
     SIGNATURE_NOT_FOUND(Status.BAD_REQUEST, 2002, "JWT 서명을 확인하지 못했습니다."),
     MALFORMED_TOKEN(Status.BAD_REQUEST, 2003, "토큰의 길이 및 형식이 올바르지 않습니다."),
-    EXPIRED_TOKEN(Status.UNAUTHORIZED, 2004, "이미 만료된 토큰입니다."),
+    EXPIRED_TOKEN(Status.UNAUTHORIZED, 2004, "이미 만료된 토큰입니다. 토큰을 재발급하시기 바랍니다."),
     UNSUPPORTED_TOKEN(Status.BAD_REQUEST, 2005, "지원되지 않는 토큰입니다."),
     INVALID_TOKEN(Status.BAD_REQUEST, 2006, "토큰이 유효하지 않습니다."),
     BAD_REQUEST_TO_PROVIDER(Status.BAD_REQUEST, 2007, "토큰이 유효하지 않습니다."),
@@ -27,16 +27,16 @@ public enum AuthExceptionType implements ExceptionType {
 
     @Override
     public Status status() {
-        return null;
+        return status;
     }
 
     @Override
     public int exceptionCode() {
-        return 0;
+        return exceptionCode;
     }
 
     @Override
     public String message() {
-        return null;
+        return message;
     }
 }
