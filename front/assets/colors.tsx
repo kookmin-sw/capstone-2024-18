c1ef8b04dae921633dcc33a31a7e2aef24074fd8
Minji Yun <gee2450@kookmin.ac.kr>
2024-03-19 23:07:46 +0900

    Feat: 구글 로그인 버튼, 커스텀 버튼 구현 - 구글 로그인 버튼, 버튼, 텍스트 버튼 테스트할 수 있는 화면(ButtonTestPage.tsx) 생성 - Style을 따로 적용하여 텍스트 버튼, 버튼 구현(테스트 화면 코드에 해당 style 코드 적음)

---
+++ front/assets/colors.tsx
@@ -0,0 +1,15 @@
+export const colors = {
+    transparent: "#00000000",
+    white: "#FFFFFF",
+    point: "#FF7269",
+    pastel_point: "#FFD0CD",
+    gray1: "#F0F0F5",
+    gray2: "#E8E8EE",
+    gray3: "#E1E1E8",
+    gray4: "#CDCED6",
+    gray5: "#A9ABB8",
+    gray6: "#858899",
+    gray7: "#525463",
+    gray8: "#505050",
+    gray9: "#2B2D36",
+}
