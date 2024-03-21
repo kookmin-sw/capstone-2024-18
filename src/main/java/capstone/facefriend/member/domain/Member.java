b2809416c322ab4104b595dede0b6b13d880593f
KimChanJin97 <cjkimhello97@gmail.com>
2024-03-21 17:18:03 +0900

    feat: Member 엔티티에서 name 필드 제거

--- src/main/java/capstone/facefriend/member/domain/Member.java
+++ src/main/java/capstone/facefriend/member/domain/Member.java
@@ -22,9 +22,6 @@ public class Member extends BaseEntity {
     private String email;
 
     @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false)
     private String password;
 
     private String imageUrl;
