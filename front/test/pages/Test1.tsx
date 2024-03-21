00dd516eec65eea3ca2299c52daa43695cc5fbf5
Minji Yun <gee2450@kookmin.ac.kr>
2024-03-22 02:44:09 +0900

    Style: 필요 없는 코드를 삭제 - 동작하지 않고 있던 코드 삭제 - 프로젝트에서 사용하지 않는 tabBarColor Screen마다 각각 설정하는 기능 코드 삭제

--- front/test/pages/Test1.tsx
+++ front/test/pages/Test1.tsx
@@ -15,28 +15,25 @@ const Test1 = () => {
         <Tab.Navigator 
           shifting={true}
           initialRouteName='sub1' 
-          activeColor="#ffffff" // 선택한 항목의 색깔
-          style={{borderTopLeftRadius: 20}} >
+          activeColor="#ffffff"
+          screenOptions={{tabBarColor: 'black'}}>
           <Tab.Screen 
             name="sub1" component={SubTest1} 
             options={{
               tabBarIcon: ({color}) => <Icon size={25} color={color} source={eye}/>,
               tabBarLabel: 'No.1',
-              tabBarColor:'black',
             }}/>
           <Tab.Screen 
             name="sub2" component={SubTest2} 
             options={{
               tabBarIcon: ({color}) => <Icon size={25} color={color} source={eye}/>,
               tabBarLabel: 'No.2',
-              tabBarColor:'#694fad'
             }}/>
           <Tab.Screen 
             name="sub3" component={SubTest2} 
             options={{
               tabBarIcon: ({color}) => <Icon size={25} color={color} source={eye}/>,
               tabBarLabel: 'No.3',
-              tabBarColor:'#FF7269'
             }}/>
         </Tab.Navigator>
       </PaperProvider>
