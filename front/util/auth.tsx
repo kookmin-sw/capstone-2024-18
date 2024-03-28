b343df7d9789f3210ff1f18c83546f9a1f8096b7
nanana3679 <nanana3679@gmail.com>
2024-03-29 00:18:00 +0900

    feat: API 메소드 구현
    - auth/find-email
    - auth/send-temporary-password
    - auth/verify-temporary-password
    - auth/verify-duplication
    - auth/send-code
    - auth/verify-code (error)
    - auth/signup
    - auth/signin
    - members/signout
    - members/reissue

--- front/util/auth.tsx
+++ front/util/auth.tsx
@@ -1,37 +1,160 @@
-import axios from "axios";
+import axios, { AxiosError } from 'axios';
 
-interface Mode {
-  mode: "SIGN_UP" | "LOG_IN"
+const LOCALHOST = "https://1308-115-138-61-136.ngrok-free.app";
+
+// 3. OK
+export const findEmail = async (email: string) => {
+  const endpoint = `${LOCALHOST}/auth/find-email?email=${email}`;
+  try {
+    const response = await axios.post(endpoint);
+    console.log(response.data);
+    const { recievedEmail, isRegistered } = response.data;
+  }
+  catch (e) {
+    console.log(e);
+  }
 }
 
-const authenticate = async (mode: Mode, userId: string, password: string) => {
-  const endpoint = "";
-  const body = {
-    userId: userId,
-    password: password
-  };
-  const response = await axios.post(endpoint, body);
-  const accessToken = response.data.accessToken;
-  console.log('Access Token:', accessToken);
-  return accessToken;
-} 
-
-const googleLogin = async () => {
-  
+// 4. OK
+export const sendTemporaryPassword = async (email: string) => {
+  const endpoint = `${LOCALHOST}/auth/send-temporary-password?email=${email}`;
   try {
-    const endpoint = "http://localhost:8080/oauth/google/login-uri?redirect-uri=http://localhost:8080/index.html";
-    const response = await axios.get(endpoint);
-    const url = new URL(response.data);
-    const params = new URLSearchParams(url.search);
-    console.log(params);
-    // const code = params.get('code');
-    // const response2 = await axios.post("http://localhost:8080/oauth/google/login", {
-    //   "redirectUri": "http://localhost:8080/index.html",
-    //   "code": code
-    // });
-    // const {access_token, refresh_token} = response2.data
-  } catch (e) {
+    const response = await axios.post(endpoint);
+    console.log(response.data);
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
 
+// 5. OK
+export const verifyTemporaryPassword = async (email: string, temPassword: string, newPassword: string, newPassword2: string) => {
+  const endpoint = `${LOCALHOST}/auth/verify-temporary-password?email=${email}&temporaryPassword=${temPassword}`;
+  const body = { newPassword, newPassword2, }
+  try {
+    const response = await axios.post(endpoint, body);
+    console.log(response.data);
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
+// 6. OK
+export const verifyDuplicationEmail = async (email: string) => {
+  const endpoint = `${LOCALHOST}/auth/verify-duplication?email=${email}`;
+  try {
+    const response = await axios.post(endpoint);
+    console.log(response.data);
+  }
+  catch (e) {
+    const axiosError = e as AxiosError;
+    if (axiosError.response && axiosError.response.status === 400) {
+      console.log("이미 사용중인 이메일입니다.");
+    }
+    else console.log(e);
+  }
+}
+
+// 7. OK
+export const sendCode = async (email: string) => {
+  const endpoint = `${LOCALHOST}/auth/send-code?email=${email}`;
+  try {
+    const response = await axios.post(endpoint);
+    console.log(response.data);
+  }
+  catch (e) {
+    const axiosError = e as AxiosError;
+    if (axiosError.response && axiosError.response.status === 400) {
+      console.log("이미 사용중인 이메일입니다.");
+    }
+    else console.log(e);
+  }
+}
+
+// 8. X
+export const verifyCode = async (email: string, code: string) => {
+  const endpoint = `${LOCALHOST}/auth/verify-code?email=${email}&code=${code}`;
+
+  try {
+    const response = await axios.post(endpoint);
+    console.log(response.data);
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
+// 9. OK
+export const signup = async (email: string, password: string, password2: string, isVerified: true) => {
+  const endpoint =  `${LOCALHOST}/auth/signup`;
+  const body = {
+    email,
+    password,
+    password2,
+    isVerified,
   }
 
+  try {
+    const response = await axios.post(endpoint, body);
+    console.log(response.data);
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
+// 10. 토큰 로직
+export const signin = async (email: string, password: string) => {
+  const endpoint =  `${LOCALHOST}/auth/signin`;
+  const body = {
+    email,
+    password,
+  }
+
+  try {
+    const response = await axios.post(endpoint, body);
+    const { accessToken, refreshToken } = response.data;
+    // token 다루는 로직
+    console.log(accessToken, refreshToken);
+  }
+  catch (e) {
+    const axiosError = e as AxiosError;
+    if (axiosError.response && axiosError.response.status === 400) {
+      console.log("이메일 또는 비밀번호가 잘못되었습니다.");
+    }
+    else console.log(e);
+  }
+}
+
+// 11. OK
+export const signout = async (accessToken: string) => {
+  const endpoint =  `${LOCALHOST}/members/signout`;
+  const config = { 
+    headers: { Authorization: 'Bearer ' + accessToken } 
+  };
+  try {
+    const response = await axios.delete(endpoint, config);
+    console.log(response.data);
+    // token 삭제 로직
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
+// 12. 토큰 구현
+export const reissue = async (accessToken: string, refreshToken: string) => {
+  const endpoint =  `${LOCALHOST}/members/reissue`;
+  const body = { refreshToken };
+  const config = { 
+    headers: { Authorization: 'Bearer ' + accessToken } 
+  };
+  try {
+    const response = await axios.post(endpoint, body, config);
+    const { receivedAccessToken, receivedRefreshToken } = response.data;
+  }
+  catch (e) {
+    console.log(e);
+  }
 }
