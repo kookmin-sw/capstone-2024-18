5fed465914008ae280f6563f69882450eab05c06
Minji Yun <gee2450@kookmin.ac.kr>
2024-03-30 21:58:15 +0900

    Refactor: Test case 추가와 - width, height, fontSize 설정할때, 글씨가 제대로 보이게 수정

--- front/components/SelectableCategory.tsx
+++ front/components/SelectableCategory.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, StyleProp, ViewStyle, TextStyle } from 'react-native';
+import { StyleSheet, View, StyleProp, ViewStyle, TextStyle, Text } from 'react-native';
 import { Chip, ChipProps } from 'react-native-paper';
 import { colors } from '../assets/colors.tsx'
 
@@ -23,15 +23,15 @@ interface SelectableProp {
 /**
  * @param selectable :SelectableProp. tag를 선택했을 경우, 색깔이 바뀌는 flag, 그리고 tag를 선택, 안 선택했을 때, 적용되는 view, font style 전달
  * @param color :string. style 없이 간단하게 카테고리의 border, text color 전달. 
- * @param children :string. 보여줄 카테고리의 텍스트 내용 전달
- * @param style :StyleProp<ViewStyle>. 카테고리에 부여할 style
+ * @param fontSize :number. style 없이 간단하게 카테고리 text의 fontSize 전달. 
+ * @param containerStyle :StyleProp<ViewStyle>. 카테고리 컨테이너에 부여할 style
  * @param textStyle :StyleProp<TextStyle>. 텍스트에 부여할 style
  */
 
 interface Props extends ChipProps {
   selectable?: SelectableProp
   color?: string
-  children: string
+  fontSize?: number
   style?: StyleProp<ViewStyle>
   textStyle?: StyleProp<TextStyle>
 }
@@ -39,7 +39,7 @@ interface Props extends ChipProps {
 const SelectableCategory = ({ 
   selectable,
   color = colors.gray9,
-  children,
+  fontSize = 14,
   style,
   textStyle,
   ...chipProps
@@ -50,12 +50,13 @@ const SelectableCategory = ({
         style={[styles.defaultTag, {borderColor: color}, style, 
           (selectable)&&((selectable.select) ? selectable.selectedStyle : selectable.unselectedStyle)
         ]} 
-        textStyle={[styles.defaultText, {color: color}, textStyle,
-          (selectable)&&((selectable.select) ? selectable.selectedTextStyle : selectable.unselectedTextStyle)
-        ]}
+        textStyle={[styles.defaultText, {color: color}, textStyle, 
+          {fontSize: fontSize, lineHeight: fontSize, height: fontSize},
+          (selectable)&&((selectable.select) ? selectable.selectedTextStyle : selectable.unselectedTextStyle)]}
         disabled={selectable?.showSelectedOnly} 
-        mode='outlined' children={children}
-        {...chipProps}/>
+        mode='outlined' 
+        {...chipProps}>
+        </Chip>
     </View>
     
   );
@@ -67,13 +68,13 @@ const styles = StyleSheet.create({
   defaultTag: {
     borderWidth: 2, 
     margin: 5,
-    alignItems: 'center',
+    padding: 2,
     borderRadius: 20,
     borderColor: colors.gray9,
     backgroundColor: colors.white,
+    justifyContent: 'center',
   },
   defaultText: {
-    fontSize: 14,
     color: colors.gray9, 
   },
   wrapper: {
--- front/pages/SelectableCategoryTestPage.tsx
+++ front/pages/SelectableCategoryTestPage.tsx
@@ -1,8 +1,7 @@
-import CustomTextInput from "../components/CustomTextInput";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import SelectableCategory from "../components/SelectableCategory";
 import { colors } from "../assets/colors";
-import { Button } from "react-native-paper";
+import { Button, PaperProvider, useTheme } from "react-native-paper";
 import { useState } from "react";
 
 
@@ -31,63 +30,110 @@ const SelectableCategoryTestPage = () => {
   const [ select, setSelect ] = useState(true);
   const [ buttonText, setButtonText ] = useState<'수정하기'|'저장하기'>('수정하기')
 
+  const theme = useTheme();
+  theme.version = 3;
+
   return (
-    <View>
+    <ScrollView showsVerticalScrollIndicator={false}>
       {/* 기본 카테고리 */}
-      <Text>기본 카테고리 Test</Text>
-      <SelectableCategory children="test"/>
+      <View style={styles.container}>
+        <Text>기본 카테고리 Test</Text>
+        <SelectableCategory children="test"/>
+      </View>
 
       {/* 기본 카테고리와 색깔 지정 */}
-      <Text>기본 카테고리+color Test</Text>
-      <SelectableCategory color={colors.point} children="test"/>
+      <View style={styles.container}>
+        <Text>기본 카테고리+color Test</Text>
+        <SelectableCategory color={colors.point} children="test"/>
+      </View>
+
+      {/* 카테고리의 크기 및 style 지정 */}
+      {/* width 지정 시, textStyle을 이렇게 설정해야 글씨가 가로 중앙 정렬 됩니다 */}
+      <View style={styles.container}>
+        <Text>카테고리의 크기 및 style Test</Text>
+        <SelectableCategory 
+          style={{width: 200, height: 100}}
+          textStyle={{flex: 1, textAlign: 'center'}} 
+          color={colors.point} children="test"/>
+      </View>
+
+      {/* 카테고리의 텍스트 크기 지정 */}
+      {/* 주의 
+        fontSize 파라미터를 이용하는 것이 아닌, 
+        textStyle을 이용해서 fontSize를 조절할 경우,
+        lineHeight를 직접 조절해야합니다. 
+       */}
+      <View style={styles.container}>
+        <Text>카테고리의 fontSize Test</Text>
+        <PaperProvider theme={theme}>
+          <SelectableCategory color={colors.point} fontSize={50}
+            children={'style'}/>
+        </PaperProvider>
+      </View>
 
       {/* 카테고리 여러 개를 사용하고 싶은 경우 */}
-      <Text>카테고리 여러 개 Test</Text>
-      <View style={{flexDirection: "row", flexWrap: "wrap"}}>
-        <SelectableCategory children="test"/>
-        <SelectableCategory children="test"/>
-        <SelectableCategory children="test"/>
-        <SelectableCategory children="test"/>
+      <View style={styles.container}>
+        <Text>카테고리 여러 개 Test</Text>
+        <View style={{flexDirection: "row", flexWrap: "wrap"}}>
+          <SelectableCategory children="test"/>
+          <SelectableCategory children="test"/>
+          <SelectableCategory children="test"/>
+          <SelectableCategory children="test"/>
+        </View>
       </View>
 
       {/* 카테고리 선택 기능을 사용하고 싶은 경우 */}
-      <Text>select 기능 Test</Text>
-      <View style={{flexDirection: "row", flexWrap: "wrap"}}>
-        <SelectableCategory children="test" onPress={() => setSelect(!select)}
-          selectable={{
-            select: select,
-            selectedStyle: {backgroundColor: colors.point, borderColor: colors.point}, unselectedStyle: {backgroundColor: colors.gray5, borderColor: colors.gray5},
-            selectedTextStyle: {color: colors.white}, unselectedTextStyle: {color: colors.white}
-          }}/>
+      <View style={styles.container}>
+        <Text>select 기능 Test</Text>
+        <View style={{flexDirection: "row", flexWrap: "wrap"}}>
+          <SelectableCategory children="test" onPress={() => setSelect(!select)}
+            selectable={{
+              select: select,
+              selectedStyle: {backgroundColor: colors.point, borderColor: colors.point}, unselectedStyle: {backgroundColor: colors.gray5, borderColor: colors.gray5},
+              selectedTextStyle: {color: colors.white}, unselectedTextStyle: {color: colors.white}
+            }}/>
+        </View>
       </View>
 
       {/* select 세부 기능을 사용하고 싶은 경우 */}
-      <Text>select 기능을 응용한 수정 기능 Test</Text>
-      <View style={{flexDirection: "row", flexWrap: "wrap"}}>
-      {
-        categories.map((item) => {
-          return (
-            <SelectableCategory children={item.text}
-              selectable={{
-                select: item.selected,
-                showSelectedOnly: !edit,
-                selectedStyle: {backgroundColor: colors.point, borderColor: colors.point}, unselectedStyle: {backgroundColor: colors.gray5, borderColor: colors.gray5},
-                selectedTextStyle: {color: colors.white}, unselectedTextStyle: {color: colors.white}
-              }}
-              onPress={() => handleCategorySelect(item.id)}
-              />
-          )
-        })
-      }
+      <View style={styles.container}>
+        <Text>select 기능을 응용한 수정 기능 Test</Text>
+        <View style={{flexDirection: "row", flexWrap: "wrap"}}>
+        {
+          categories.map((item) => {
+            return (
+              <SelectableCategory children={item.text} key={item.id}
+                selectable={{
+                  select: item.selected,
+                  showSelectedOnly: !edit,
+                  selectedStyle: {backgroundColor: colors.point, borderColor: colors.point}, unselectedStyle: {backgroundColor: colors.gray5, borderColor: colors.gray5},
+                  selectedTextStyle: {color: colors.white}, unselectedTextStyle: {color: colors.white}
+                }}
+                onPress={() => handleCategorySelect(item.id)}
+                />
+            )
+          })
+        }
+        </View>
+        <Button buttonColor={colors.point} textColor={colors.white} children={buttonText}
+          onPress={() => {
+            setEdit(!edit); 
+            if (buttonText == '수정하기') { setButtonText('저장하기');}
+            else { setButtonText('수정하기'); }
+          }}/>
       </View>
-      <Button buttonColor={colors.point} textColor={colors.white} children={buttonText}
-        onPress={() => {
-          setEdit(!edit); 
-          if (buttonText == '수정하기') { setButtonText('저장하기');}
-          else { setButtonText('수정하기'); }
-        }}/>
-    </View>
+    </ScrollView>
   )
 }
 
 export default SelectableCategoryTestPage;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.gray2,
+    margin: 10, 
+    paddingHorizontal: 10, 
+    paddingVertical: 20, 
+    borderRadius: 20
+  }
+});
