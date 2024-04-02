9dca7ff8d82dff9e15570fa3d834fa70f8afc7c7
Minji Yun <gee2450@kookmin.ac.kr>
2024-04-03 00:16:53 +0900

    Refactor: 버튼의 height를 입력 받아, 크기 조절하도록 코드 수정 - 버튼의 height를 입력 받아, 크기 조절하도록 코드 수정 - 버튼의 background, text의 fontSize도 필수록 받도록 코드 수정

--- front/components/CustomButton.tsx
+++ front/components/CustomButton.tsx
@@ -1,18 +1,28 @@
-import { TouchableOpacity, ViewStyle, TouchableOpacityProps, StyleProp, TextStyle, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, ViewStyle, TouchableOpacityProps, StyleProp, TextStyle, Text, StyleSheet, DimensionValue, ColorValue } from 'react-native';
 import { colors } from '../assets/colors';
+
+
+interface BtnContainerStyle extends ViewStyle {
+  height: DimensionValue | undefined
+  backgroundColor: ColorValue | undefined;
+}
+
+interface BtnTextStyle extends TextStyle {
+  fontSize: number | undefined;
+}
 
 
 interface Props extends TouchableOpacityProps {
   type?: 'default' | 'fit-content'
   children: string
-  containerStyle?: StyleProp<ViewStyle>,
-  textStyle?: StyleProp<TextStyle>
+  containerStyle: StyleProp<BtnContainerStyle>,
+  textStyle: StyleProp<BtnTextStyle>
 }
 
 /**
  * @param type :'default'|'fit-content'. 버튼의 type을 지정
  * @param children :string. 버튼의 text
- * @param containerStyle :StyleProp<ViewStyle>. container에 부여할 style
+ * @param containerStyle :StyleProp<BtnViewStyle>. container에 부여할 style
  * @param textStyle :StyleProp<TextStyle>. text에 부여할 style
  */
 const CustomButton = ({ 
@@ -23,46 +33,12 @@ const CustomButton = ({
   ...touchableOpacityProps
 } : Props) => {
   const defaultFontSize = 14;
-  function getButtonHeight(fontSize: number) {
-    if (type == 'default') {
-      return fontSize * 2.4;
-    } else {
-      return fontSize * 1.5;
-    }
-  }
-  
-  // 사용자가 설정한 textStyle에서 fontSize 설정한 값이 있으면 가져오기.
-  // 설정한 fontSize가 없으면, 자동으로, 기본 설정 fontSize를 사용합니다. 
-  let fontSize = defaultFontSize;
-  if (Array.isArray(textStyle)) {
-    if (textStyle[0] && typeof textStyle[0] === 'object' && 'fontSize' in textStyle[0]) {
-      fontSize = textStyle[0].fontSize;
-    }
-  } else {
-    if (typeof textStyle === 'object' && textStyle && 'fontSize' in textStyle) {
-      fontSize = textStyle?.fontSize;
-    }
-  }
-
-
-  // 사용자가 설정한 containerSytle에서 height 가져오기. 
-  // 설정한 height가 없으면, 자동으로, 버튼의 height는 fontSize를 기반해서 정해집니다. 
-  let height = getButtonHeight(fontSize);
-  if (Array.isArray(containerStyle)) {
-    if (containerStyle[0]?.height) {
-      height = Math.max(height, containerStyle[0]?.height);
-    }
-  } else {
-    if (containerStyle?.height) {
-      height = Math.max(height, containerStyle?.height);
-    }
-  }
 
   if (type == 'default') {
     return (
       <TouchableOpacity style={[styles.defaultContainerStyle, 
-        containerStyle, {height: height}]}>
-        <Text style={[styles.defaultTextStyle, {fontSize: fontSize}, textStyle]}>
+        containerStyle]}>
+        <Text style={[styles.defaultTextStyle,  textStyle]}>
           {children}
         </Text>
       </TouchableOpacity>
@@ -71,8 +47,8 @@ const CustomButton = ({
   else { // fit-content button. 클릭할 수 있는 text button type
     return (
       <TouchableOpacity style={[styles.fitContentContainerStyle, 
-        containerStyle, {height: height}]}>
-        <Text style={[styles.fitContentTextStyle, {fontSize: fontSize}, textStyle]}>
+        containerStyle]}>
+        <Text style={[styles.fitContentTextStyle, textStyle]}>
           {children}
         </Text>
       </TouchableOpacity>
