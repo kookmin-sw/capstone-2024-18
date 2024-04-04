d6ae8c9f3dc16d7f3a31c81cd063be6643f81b87
nanana3679 <nanana3679@gmail.com>
2024-04-04 16:29:01 +0900

    feat: CustomSlider 컴포넌트 구현
    - @miblanchard/react-native-slider 사용
    - 디자인 수정 필요

---
+++ front/components/CustomSlider.tsx
@@ -0,0 +1,33 @@
+import { Slider } from "@miblanchard/react-native-slider"
+import { View, Text } from "react-native"
+import { useState, useEffect } from "react"
+import { colors } from "../assets/colors"
+
+interface Props {
+  values: string[];
+  setValue: (value: string) => void;
+}
+
+const CustomSlider = ({ values, setValue }: Props) => {
+
+  return (
+    <View style={{ width: values.length * 50 }}>
+      <View style={{ paddingHorizontal: 5 }}>
+        <Slider 
+          minimumValue={0}
+          maximumValue={values.length - 1}
+          step={1}
+          minimumTrackTintColor={colors.gray4}
+          maximumTrackTintColor={colors.gray4}
+          thumbTintColor={colors.pastel_point}
+          onValueChange={(sliderValue) => setValue(values[sliderValue[0]])}
+        />
+      </View>
+        <View style={{ flexDirection: "row", justifyContent: "space-between"}}>
+          {values.map( value => <Text key={value} style={{color: colors.gray7}}>{value}</Text>)}
+        </View>  
+    </View>
+  )
+}
+
+export default CustomSlider;
