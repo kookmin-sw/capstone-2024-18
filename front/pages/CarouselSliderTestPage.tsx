4ec7493ada4a7fe5b890f7190ca40d99835d0475
Minji Yun <gee2450@kookmin.ac.kr>
2024-04-09 15:38:28 +0900

    Feat: pageHeight 추가와 slider style 코드 수정 - renderItem의 offset, gap, pageWidth를 컴포넌트 안에서 조정 - edit 변수로 icon, borderRadius 테스트코드 추가

--- front/pages/CarouselSliderTestPage.tsx
+++ front/pages/CarouselSliderTestPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import CarouselSlider from "../components/CarouselSlider";
+import { CarouselSlider, GapProps} from "../components/CarouselSlider";
 import ImageWithIconOverlay from "../components/ImageWithIconOverlay";
-import { Dimensions } from "react-native";
+import { Dimensions, ImageSourcePropType, StyleProp, ViewStyle } from "react-native";
 import { colors } from "../assets/colors";
 
 const CarouselSliderTestPage = () => {
@@ -13,25 +13,18 @@ const CarouselSliderTestPage = () => {
   /**
    * auto: 자동으로 페이지가 넘어가는 기능을 보고 싶으면 true, 아니면 false 설정
    */
-  const auto = true;
+  const auto = false;
+
+  /**
+   * edit: 수정 기능을 보고 싶으면 true, 아니면 false 설정
+   */
+  const edit = false;
 
   // component의 필수 파라미터, pageWidth, offset, gap 설정
-  const { width } = Dimensions.get('window');
+  const width = Dimensions.get('window').width;
   const [ pageWidth, setPageWidth ] = useState(width);
   const [ offset, setOffset ] = useState(0);
-  const [ gap, setGap ] = useState(0.0);
-
-  useEffect(() => {
-    if (flag) {
-      setPageWidth(width);
-      setOffset(0);
-      setGap(0);
-    } else {
-      setPageWidth(width*0.6);
-      setOffset(Math.round((width - width*0.6) / 4));
-      setGap(Math.round((width - width*0.6) / 4));
-    }
-  }, [flag])
+  const [ gap, setGap ] = useState({leftGap: 0, rightGap: 0, topGap: 0, bottomGap: 0});
 
   // 내부 page 설정
   const images = [
@@ -55,42 +48,65 @@ const CarouselSliderTestPage = () => {
       type: 'image',
       source: require('../assets/images/imageTest1.png')
     },
-    {
-      id: 4,
-      type: 'add',
-      source: null
-    }
+    // {
+    //   id: 4,
+    //   type: 'add',
+    //   source: null
+    // }
   ]
-  function renderItem({item}: any) {
-    const defaultContainerStyle = {
-      width: pageWidth, 
-      height: pageWidth, 
-      marginHorizontal: gap / 2, 
-      marginTop: gap
-    };
-
-    if (item.type == 'image') {
+  function renderItem(
+    {id, type, source}: {id: number, type: string, source: ImageSourcePropType | undefined},
+    containerStyle: StyleProp<ViewStyle>) {
+    if (type == 'image') {
       return (
         <ImageWithIconOverlay
-          source={item.source} borderRadius={15} key={item.id}
-          containerStyle={defaultContainerStyle}
-          rightIcon={{source: require('../assets/images/Icon.png')}} 
+          source={source} borderRadius={edit?15:0} key={id}
+          containerStyle={containerStyle}
+          rightIcon={edit?{source: require('../assets/images/Icon.png')}:undefined} 
           rightPressable={{onPress: () => {console.log("right press")}}}
           />
       );
     } else{
       return (
         <ImageWithIconOverlay
-          borderRadius={15} key={item.id}
-          containerStyle={[defaultContainerStyle, {backgroundColor: colors.gray2}]}
+          borderRadius={15} key={id}
+          containerStyle={[containerStyle, {backgroundColor: colors.gray2}]}
           centerIcon={{size: 80, source: 'plus'}} centerPressable={{onPress: () => {}, style:{alignSelf: 'center'}}}/>
       );
     } 
   }
 
+  useEffect(() => {
+    if (flag) {
+      setPageWidth(width);
+      setOffset(0);
+      setGap({leftGap: 0, rightGap: 0, topGap: 0, bottomGap: 0});
+    } else {
+      const _offset = 37;
+      const _gap = 20;
+
+      setPageWidth(width - _offset * 2 - _gap * 2);
+      setOffset(_offset);
+      setGap({leftGap: _gap, rightGap: _gap, topGap: 46, bottomGap: 20});
+    }
+  }, [flag])
+
+  useEffect(() => {
+    if (auto) {
+      if (images[images.length-1].type == 'add') {
+        images.pop()
+      }
+    } else {
+      if (images[images.length-1].type != 'add') {
+        images.push({id: images.length, type: 'add', source: null})
+      }
+    }
+  }, [auto])
+
   return (
     <CarouselSlider
       pageWidth={pageWidth}
+      pageHeight={pageWidth}
       offset={offset}
       gap={gap}
       data={images}
