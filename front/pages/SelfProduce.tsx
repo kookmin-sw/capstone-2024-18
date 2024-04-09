267c383ed7ee3dcecff92b09658b619cbf334980
Minji Yun <gee2450@kookmin.ac.kr>
2024-04-09 18:37:36 +0900

    Feat: Selelctable Tag, 이미지 슬라이더 기능 자기소개서 화면에 구현 - 이미지 슬라이더, selectable tag 구현 - 이미지 추가와 삭제 기능 구현

--- front/pages/SelfProduce.tsx
+++ front/pages/SelfProduce.tsx
@@ -1,14 +1,16 @@
-import { View, Text, ScrollView, StyleSheet, Dimensions, Alert } from 'react-native';
-import { ImageSlider } from "react-native-image-slider-banner";
+import { View, Text, ScrollView, StyleSheet, Dimensions, Alert, StyleProp, ViewStyle, ImageSourcePropType } from 'react-native';
 import { Icon, Chip } from 'react-native-paper';
 import { colors } from '../assets/colors.tsx'
 import CustomButton from '../components/CustomButton';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CustomTextInput from '../components/CustomTextInput.tsx';
+import ImageWithIconOverlay from '../components/ImageWithIconOverlay.tsx';
+import CarouselSlider from '../components/CarouselSlider.tsx';
+import SelectableTag from '../components/SelectableTag.tsx';
 
 
 const SelfProduce = () => {
-  const { width } = Dimensions.get('window');
+  const width = Dimensions.get('window').width;
 
   const selectedColor = colors.point;
   const unselectedColor = colors.white;
@@ -16,6 +18,67 @@ const SelfProduce = () => {
   const unselectedFontColor = "#9E9E9E";
   const selectedBorderColor = colors.point;
   const unselectedBorderColor = "#9E9E9E";
+
+  // component의 필수 파라미터, pageWidth, offset, gap 설정
+  const [ pageWidth, setPageWidth ] = useState(width);
+  const [ offset, setOffset ] = useState(0);
+  const [ gap, setGap ] = useState({leftGap: 0, rightGap: 0, topGap: 0, bottomGap: 0});
+
+  const [ page, setPage ] = useState(0);
+
+  // edit 기능 조절 변수
+  const [ edit, setEdit ] = useState(false);
+
+  // 내부 page 설정
+  const [ images, setImages ] = useState([
+    {
+      id: 0,
+      type: 'image',
+      source: require('../assets/images/imageTest1.png'),
+    },
+    {
+      id: 1,
+      type: 'image',
+      source: require('../assets/images/imageTest1.png')
+    },
+    {
+      id: 2,
+      type: 'image',
+      source: require('../assets/images/imageTest1.png')
+    },
+    {
+      id: 3,
+      type: 'image',
+      source: require('../assets/images/imageTest1.png')
+    }
+  ]);
+
+  function renderItem(
+    {id, type, source}: {id: number, type: string, source: ImageSourcePropType | undefined},
+    containerStyle: StyleProp<ViewStyle>) {
+    if (type == 'image') {
+      return (
+        <ImageWithIconOverlay
+          source={source} borderRadius={edit?15:0} key={id}
+          containerStyle={containerStyle}
+          rightIcon={edit?{source: require('../assets/images/Icon.png')}:undefined} 
+          rightPressable={{onPress: () => {handleDeleteImageAtIndex(id)}}}/>
+      );
+    } else{
+      return (
+        <ImageWithIconOverlay
+          borderRadius={15} key={id}
+          containerStyle={[containerStyle, {backgroundColor: colors.gray2}]}
+          centerIcon={{size: 80, source: 'plus'}} centerPressable={{onPress: () => takePhoto(), style:{alignSelf: 'center'}}}/>
+      );
+    } 
+  }
+
+  function takePhoto() {
+    console.log("take photo");
+    handleAddImageAtIndex(images.length-1, {id: images.length, type: 'image', source: require('../assets/images/imageTest1.png')
+    })
+  }
 
   const [ haveSelfProduce, setHaveSelfProduce ] = useState(true);
 
@@ -31,7 +94,6 @@ const SelfProduce = () => {
     {id: 2, text: "모험심이 강함"},
     {id: 3, text: "영리함"}
   ])
-  const [ edit, setEdit ] = useState(false);
   const [ categories, setCategories ] = useState([
     {id: 0, text: "운동", selected: true},
     {id: 1, text: "음식", selected: false},
@@ -73,7 +135,6 @@ const SelfProduce = () => {
           onPress: () => deleteSelfProduce()
         }
       ])
-
   }
 
   function deleteSelfProduce() {
@@ -83,38 +144,95 @@ const SelfProduce = () => {
     // 자기소개서 삭제하는 요청 전송
   }
 
+  const handleAddImageAtIndex = (index: number, newImage: any) => {
+    // splice() 메서드를 사용하여 데이터의 특정 인덱스에 새로운 원소를 추가합니다.
+    const newImages = [...images];
+    newImages.splice(index, 0, newImage);
+
+    // 변경된 배열을 상태로 업데이트합니다.
+    setImages(newImages);
+  };
+
+  const handleDeleteImageAtIndex = (index: number) => {
+    // splice() 메서드를 사용하여 데이터의 특정 인덱스에 새로운 원소를 추가합니다.
+    const newImages = [...images];
+    newImages.splice(index, 1);
+
+    // 변경된 배열을 상태로 업데이트합니다.
+    setImages(newImages);
+  };
+
+  // edit 기능일 때, 이미지 추가하는 페이지 추가
+  useEffect(() => {
+    if (!edit) {
+      if (images[images.length-1].type == 'add') {
+        images.pop()
+      }
+    } else {
+      if (images[images.length-1].type != 'add') {
+        console.log('test')
+        // images.push({id: images.length, type: 'add', source: null})
+        handleAddImageAtIndex(images.length, {id: images.length, type: 'add', source: null});
+      }
+    }
+  }, [edit])
+
+  useEffect(() => {
+    if (!edit) {
+      setPageWidth(width);
+      setOffset(0);
+      setGap({leftGap: 0, rightGap: 0, topGap: 0, bottomGap: 0});
+    } else {
+      const _offset = 37;
+      const _gap = 20;
+
+      setPageWidth(width - _offset * 2 - _gap * 2);
+      setOffset(_offset);
+      setGap({leftGap: _gap, rightGap: _gap, topGap: 46, bottomGap: 20});
+    }
+  }, [edit])
+
   return (
-    (haveSelfProduce == false) ? 
-      <>
-        <View style={styles.container}>
-
-          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Icon size={150} source={require('../assets/images/surprise.png')}/>
-            <View style={{marginTop: 20, alignItems: 'center'}}>
-              <Text>자기소개서가 존재하지 않습니다.</Text>
-              <Text>나를 소개해보세요</Text>
-            </View>
+    (haveSelfProduce == false) 
+      ? 
+      <View style={styles.container}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Icon size={150} source={require('../assets/images/surprise.png')}/>
+          <View style={{marginTop: 20, alignItems: 'center'}}>
+            <Text>자기소개서가 존재하지 않습니다.</Text>
+            <Text>나를 소개해보세요</Text>
           </View>
-          <CustomButton 
-            styles={{backgroundColor: colors.point}}
-            onPress={()=>{setHaveSelfProduce(!haveSelfProduce)}}>
-            <Text style={{color: colors.white}}>자기소개서 작성하기</Text>
-          </CustomButton>
         </View>
-
-      </> :
-    
+        <CustomButton 
+          containerStyle={{backgroundColor: colors.point}}
+          onPress={()=>{setHaveSelfProduce(!haveSelfProduce)}}
+          textStyle={{color: colors.white}}>자기소개서 작성하기
+        </CustomButton>
+      </View>
+      : 
       <ScrollView showsVerticalScrollIndicator={false} style={{backgroundColor: "#F5F5F5"}}>
-        {/* 사진 편집 기능 때문에, ImageSlider 따로 구현할 예정이기 때문에, 아무 slider 씁니다 */}
-        <ImageSlider 
-          data={[
-            {img: require('../assets/images/imageTest1.png')},
-            {img: require('../assets/images/imageTest1.png')},
-            {img: require('../assets/images/imageTest1.png')},
-          ]}
-          localImg={true}
-          caroselImageStyle={{ resizeMode: 'cover', height: width}}
-        />
+        {/* 이미지 슬라이더 */}
+        <CarouselSlider
+          pageWidth={pageWidth}
+          pageHeight={pageWidth}
+          offset={offset}
+          gap={gap}
+          data={images}
+          onPageChange={setPage}
+          renderItem={renderItem}/>
+        
+        <View style={{flexDirection: 'row', alignSelf: 'center', paddingTop: 10}}>
+        {
+          images.map((item, idx) => {
+            return (
+              <View 
+                key={idx}
+                style={{marginHorizontal: 4, width: 8, height: 8, borderRadius: 50,
+                backgroundColor: (idx == page) ? colors.pastel_point : colors.gray2}}/>
+            );
+          })
+        }
+        </View>
 
         {/* 프로필 사진, 이름 섹션 */}
         <View style={styles.container} >
@@ -132,13 +250,13 @@ const SelfProduce = () => {
             </View>
             <View style={styles.tagContainer}>
             {
-              basic.map((item, idx) => {
+              basic.map((item) => {
                 return (
-                  <View key={item.id} style={styles.tagWrapper}>
-                    <Chip 
-                      style={styles.uneditableTag} textStyle={styles.uneditableText}
-                      children={item.text}/>
-                  </View>
+                  <SelectableTag 
+                    key={item.id}
+                    touchAreaStyle={{marginRight: 6, marginBottom: 6}}
+                    containerStyle={styles.uneditableTag} 
+                    textStyle={styles.uneditableText} children={item.text}/>
                 );
               })
             }
@@ -156,11 +274,12 @@ const SelfProduce = () => {
             {
               face.map((item) => {
                 return (
-                  <View key={item.id} style={styles.tagWrapper}>
-                    <Chip 
-                      style={styles.uneditableTag} textStyle={styles.uneditableText}
-                      children={item.text}/>
-                  </View>
+                  <SelectableTag 
+                    key={item.id}
+                    touchAreaStyle={{marginRight: 6, marginBottom: 6}}
+                    height={25}
+                    containerStyle={styles.uneditableTag} 
+                    textStyle={styles.uneditableText} children={item.text}/>
                 );
               })
             }
@@ -176,18 +295,20 @@ const SelfProduce = () => {
             {
               categories.map((item) => {
                 return (
-                  <View key={item.id} style={[styles.tagWrapper, {display: (edit || item.selected) ? 'flex': 'none'}]}>
-                    <Chip 
-                      style={[styles.editableTag, {
-                        backgroundColor: item.selected ? selectedColor : unselectedColor, 
-                        borderColor: item.selected ? selectedBorderColor : unselectedBorderColor
-                      }]} 
-                      textStyle={[styles.editableText, {
-                        color: item.selected ? selectedFontColor : unselectedFontColor, 
-                      }]}
-                      mode='outlined' children={item.text}
-                      onPress={() => {handleCategorySelect(item.id)}} />
-                  </View> 
+                  <SelectableTag 
+                    key={item.id}
+                    touchAreaStyle={{marginRight: 6, marginBottom: 6}}
+                    height={25}
+                    selectable={{
+                      select: item.selected, showSelectedOnly: !edit,
+                      selectedStyle: {backgroundColor: colors.point, borderColor: colors.point},
+                      unselectedStyle: {backgroundColor: colors.gray5, borderColor: colors.gray5},
+                      selectedTextStyle: {color: colors.white},
+                      unselectedTextStyle: {color: colors.white}
+                    }}
+                    containerStyle={styles.uneditableTag} 
+                    onPress={() => {handleCategorySelect(item.id)}}
+                    textStyle={styles.uneditableText} children={item.text}/>
                 );
               })
             }
@@ -203,23 +324,22 @@ const SelfProduce = () => {
               containerStyle={styles.inputContainer} 
               style={{flex: 1, color: colors.gray9}} multiline={true}
               editable={edit ? true : false}
-              onChangeText={(text) => {setEssay(text);}}
+              onChangeText={(text) => {setEssay(text)}}
               children={essay}/>
           </View>
 
           <View style={{...styles.section, flexDirection: 'row'}}>
             <View style={{width: "50%", display: edit ? 'none' : 'flex'}}>
-              <CustomButton styles={{backgroundColor: colors.gray4, marginHorizontal: 5}} onPress={() => {deleteAlert()}}>
-                <Text style={{color: colors.white}}>삭제하기</Text>
+              <CustomButton 
+                containerStyle={{backgroundColor: colors.gray4, marginHorizontal: 5}} onPress={() => {deleteAlert()}}
+                textStyle={{color: colors.white}}>삭제하기
               </CustomButton>
             </View>
             <View style={{width: edit ? "100%" : "50%"}}>
-              <CustomButton styles={{backgroundColor: colors.point, marginHorizontal: 5}} onPress={() => {setEdit(!edit)}}>
-              {
-                edit ? 
-                  <Text style={{color: colors.white}}>완료</Text> : 
-                  <Text style={{color: colors.white}}>수정하기</Text>
-              }
+              <CustomButton 
+                containerStyle={{backgroundColor: colors.point, marginHorizontal: 5}} onPress={() => {setEdit(!edit)}}
+                textStyle={{color: colors.white}}>
+                {edit ? "완료" : "수정하기"}
               </CustomButton>
             </View>
           </View>
@@ -240,23 +360,33 @@ const styles = StyleSheet.create({
     alignSelf: 'center'
   },
   uneditableTag: {
-    borderWidth: 1.6, 
+    borderWidth: 1, 
     borderColor: colors.point,
     backgroundColor: colors.white,
     alignItems: 'center',
     borderRadius: 20
   },
-  editableTag: {
-    borderWidth: 2, 
+  unselectedTag: {
+    borderWidth: 1, 
+    borderColor: colors.point,
+    backgroundColor: colors.point,
     alignItems: 'center',
     borderRadius: 20
   },
+  selectedTag: {
+
+  },
+  unselectedText: {
+
+  },
+  selectedText: {
+
+  },
   uneditableText: {
     color: colors.gray7, 
-    fontSize: 14
-  },
-  editableText: {
-    fontSize: 14
+    fontSize: 14,
+    marginLeft: 9,
+    marginRight: 9
   },
   section: {
     paddingTop: 20
@@ -281,7 +411,8 @@ const styles = StyleSheet.create({
     marginVertical: 10, 
     marginHorizontal:0, 
     padding: 15, 
-    borderRadius: 15
+    borderRadius: 15,
+    height: undefined
   },
   tagContainer: {
     flexDirection: "row", 
