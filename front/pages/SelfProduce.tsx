8e9b1a6079f1841f678df3cb70b14847a0625037
Minji Yun <gee2450@kookmin.ac.kr>
2024-04-14 03:09:03 +0900

    Comment: 캐러셀슬라이더, SelfProduce 코드 정리 및 주석 추가 - SelfProduce에 unique한 key를 위해 uuid 사용

--- front/pages/SelfProduce.tsx
+++ front/pages/SelfProduce.tsx
@@ -1,68 +1,87 @@
-import { View, Text, ScrollView, StyleSheet, Dimensions, Alert, StyleProp, ViewStyle, ImageSourcePropType } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Dimensions, Alert, StyleProp, ViewStyle, ImageSourcePropType, Modal } from 'react-native';
 import { Icon, Chip } from 'react-native-paper';
 import { colors } from '../assets/colors.tsx'
 import CustomButton from '../components/CustomButton';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CustomTextInput from '../components/CustomTextInput.tsx';
 import ImageWithIconOverlay from '../components/ImageWithIconOverlay.tsx';
 import CarouselSlider from '../components/CarouselSlider.tsx';
 import SelectableTag from '../components/SelectableTag.tsx';
+import { showModal } from '../components/CameraComponent.tsx';
+
+// 이미지들의 고유 key를 임시로 주기 위한 라이브러리
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
 
 
 const SelfProduce = () => {
+  const [ haveSelfProduce, setHaveSelfProduce ] = useState(true);
+
+  // 자기소개서 edit 기능 조절 변수
+  const [ edit, setEdit ] = useState(false);
+
+  // CarouselSlider의 필수 파라미터, pageWidth, offset, gap 설정
   const width = Dimensions.get('window').width;
-
-  const selectedColor = colors.point;
-  const unselectedColor = colors.white;
-  const selectedFontColor = colors.white;
-  const unselectedFontColor = "#9E9E9E";
-  const selectedBorderColor = colors.point;
-  const unselectedBorderColor = "#9E9E9E";
-
-  // component의 필수 파라미터, pageWidth, offset, gap 설정
   const [ pageWidth, setPageWidth ] = useState(width);
   const [ offset, setOffset ] = useState(0);
   const [ gap, setGap ] = useState({leftGap: 0, rightGap: 0, topGap: 0, bottomGap: 0});
 
+  // 현재 이미지 슬라이더의 페이지를 알기 위한 페이지 설정
   const [ page, setPage ] = useState(0);
 
-  // edit 기능 조절 변수
-  const [ edit, setEdit ] = useState(false);
+  // 이미지 추가하는 방식 모달 가시성 설정
+  const [ modalVisible, setModalVisible ] = useState(false);
 
-  // 내부 page 설정
+  // 이미지 슬라이더의 내부 contents 설정
   const [ images, setImages ] = useState([
     {
-      id: 0,
-      type: 'image',
-      source: require('../assets/images/imageTest1.png'),
+      id: uuidv4(),
+      type: 'basic',
+      source: require('../assets/images/Jenny_image.png'),
     },
     {
-      id: 1,
+      id: uuidv4(),
+      type: 'basic',
+      source: require('../assets/images/Jenny_image.png')
+    },
+    {
+      id: uuidv4(),
       type: 'image',
       source: require('../assets/images/imageTest1.png')
     },
-    {
-      id: 2,
-      type: 'image',
-      source: require('../assets/images/imageTest1.png')
-    },
-    {
-      id: 3,
-      type: 'image',
-      source: require('../assets/images/imageTest1.png')
-    }
   ]);
 
-  function renderItem(
-    {id, type, source}: {id: number, type: string, source: ImageSourcePropType | undefined},
+  function setPhoto(uri: string) {
+    handleAddImageAtIndex(images.length-1, {id: images.length, type: 'image', source: {uri: uri}})
+  }
+
+  // 이미지 추가하는 방식(사진 찍기, 갤러리에서 사진 가져오기) 모달 보여주기
+  function takePhoto() {
+    setModalVisible(true);
+  }
+
+  /**
+   * 이미지 슬라이더에 들어갈 컨텐츠 내용물 데이터를 React.ReactNode로 바꿔주는 함수
+   * @param param0 :any
+   * @param containerStyle :이미지 슬라이더가 gap, offset 등을 설정할 수 있게, style을 파라미터로 받아서, 알맞은 ReactNode에 적용
+   * @returns React.ReactNode
+   */
+  function renderItem({id, type, source}: any,
     containerStyle: StyleProp<ViewStyle>) {
-    if (type == 'image') {
+    if (type === 'basic') {
+      return (
+        <ImageWithIconOverlay
+          source={source} borderRadius={edit?15:0} key={id}
+          containerStyle={containerStyle}/>
+      );
+    }
+    else if (type === 'image') {
       return (
         <ImageWithIconOverlay
           source={source} borderRadius={edit?15:0} key={id}
           containerStyle={containerStyle}
           rightIcon={edit?{source: require('../assets/images/Icon.png')}:undefined} 
-          rightPressable={{onPress: () => {handleDeleteImageAtIndex(id)}}}/>
+          rightPressable={{onPress: () => {handleDeleteImageWidthId(id)}}}/>
       );
     } else{
       return (
@@ -74,14 +93,49 @@ const SelfProduce = () => {
     } 
   }
 
-  function takePhoto() {
-    console.log("take photo");
-    handleAddImageAtIndex(images.length-1, {id: images.length, type: 'image', source: require('../assets/images/imageTest1.png')
-    })
-  }
+  const handleAddImageAtIndex = (index: number, newImage: any) => {
+    // splice() 메서드를 사용하여 데이터의 특정 인덱스에 새로운 원소를 추가합니다.
+    const newImages = [...images];
+    newImages.splice(index, 0, newImage);
 
-  const [ haveSelfProduce, setHaveSelfProduce ] = useState(true);
+    // 변경된 배열을 상태로 업데이트합니다.
+    setImages(newImages);
+  };
 
+  const handleDeleteImageWidthId = (id: string) => {
+    const newData = images.filter(image=> image.id !== id);
+    setImages(newData);
+  };
+
+  // edit 기능 설정하고 저장할 때, 이미지 추가 버튼 생성, 삭제, 이미지 슬라이더의 gap, offset, page width 재설정
+  useEffect(() => {
+    if (!edit) {
+      // 이미지 추가 페이지 삭제
+      if (images[images.length-1].type == 'add') {
+        images.pop()
+      }
+
+      // page width, offset, gap 재설정
+      setPageWidth(width);
+      setOffset(0);
+      setGap({leftGap: 0, rightGap: 0, topGap: 0, bottomGap: 0});
+    } else {
+      // 이미지 추가 페이지 생성
+      if (images[images.length-1].type != 'add') {
+        console.log('test')
+        handleAddImageAtIndex(images.length, {id: uuidv4(), type: 'add', source: null});
+      }
+
+      // page width, offset, gap 재설정
+      const _offset = 37;
+      const _gap = 20;
+      setPageWidth(width - _offset * 2 - _gap * 2);
+      setOffset(_offset);
+      setGap({leftGap: _gap, rightGap: _gap, topGap: 46, bottomGap: 20});
+    }
+  }, [edit])
+
+  // 기본 정보, 관상 정보, 카테고리 정보, 자기소개서 내용
   const [ basic, setBasic ] = useState([
     {id: 0, text: "여성"},
     {id: 1, text: "20대 중반"},
@@ -106,6 +160,7 @@ const SelfProduce = () => {
   ])
   const [ essay, setEssay ] = useState('안녕하세요! 저는 Anna 이에요!\n저는 서울 소재 대학교에서 4학년 재학 중인 Jenny에요!\n저는 평일에 학교 근처에서 복싱장을 같이 다닐 사람을 구하고 있어요! 혼자 다니면 금방 질려서 그만두게 되더라구요.\n복싱장을 다녀온 이후에는 가볍게 맥주 한잔 정도는 괜찮은 것 같아요!\n\n대화를 나눠보고 찐친이 될 가능성이 보인다면 AI 관상을 지울게요~~');
 
+  // 특정 인덱스의 카테고리를 선택, 선택 취소
   function handleCategorySelect(changeIdx: number) {
     const nextCategory = categories.map((category) => {
       if (category.id === changeIdx) {
@@ -121,9 +176,10 @@ const SelfProduce = () => {
     setCategories(nextCategory);
   }
 
+  // 자기소개서 삭제 관련 기능
   function deleteAlert() {
     Alert.alert(
-      "정말 자기소개서를 삭제하시겠습니까?", 
+      "정말 삭제하시겠습니까?", 
       "삭제하면, 해당 자기소개서를 복구할 수 없습니다",
       [
         {
@@ -144,54 +200,6 @@ const SelfProduce = () => {
     // 자기소개서 삭제하는 요청 전송
   }
 
-  const handleAddImageAtIndex = (index: number, newImage: any) => {
-    // splice() 메서드를 사용하여 데이터의 특정 인덱스에 새로운 원소를 추가합니다.
-    const newImages = [...images];
-    newImages.splice(index, 0, newImage);
-
-    // 변경된 배열을 상태로 업데이트합니다.
-    setImages(newImages);
-  };
-
-  const handleDeleteImageAtIndex = (index: number) => {
-    // splice() 메서드를 사용하여 데이터의 특정 인덱스에 새로운 원소를 추가합니다.
-    const newImages = [...images];
-    newImages.splice(index, 1);
-
-    // 변경된 배열을 상태로 업데이트합니다.
-    setImages(newImages);
-  };
-
-  // edit 기능일 때, 이미지 추가하는 페이지 추가
-  useEffect(() => {
-    if (!edit) {
-      if (images[images.length-1].type == 'add') {
-        images.pop()
-      }
-    } else {
-      if (images[images.length-1].type != 'add') {
-        console.log('test')
-        // images.push({id: images.length, type: 'add', source: null})
-        handleAddImageAtIndex(images.length, {id: images.length, type: 'add', source: null});
-      }
-    }
-  }, [edit])
-
-  useEffect(() => {
-    if (!edit) {
-      setPageWidth(width);
-      setOffset(0);
-      setGap({leftGap: 0, rightGap: 0, topGap: 0, bottomGap: 0});
-    } else {
-      const _offset = 37;
-      const _gap = 20;
-
-      setPageWidth(width - _offset * 2 - _gap * 2);
-      setOffset(_offset);
-      setGap({leftGap: _gap, rightGap: _gap, topGap: 46, bottomGap: 20});
-    }
-  }, [edit])
-
   return (
     (haveSelfProduce == false) 
       ? 
@@ -210,6 +218,8 @@ const SelfProduce = () => {
         </CustomButton>
       </View>
       : 
+    <>
+      {showModal(modalVisible, () => {setModalVisible(false)}, setPhoto )}
       <ScrollView showsVerticalScrollIndicator={false} style={{backgroundColor: "#F5F5F5"}}>
         {/* 이미지 슬라이더 */}
         <CarouselSlider
@@ -220,11 +230,12 @@ const SelfProduce = () => {
           data={images}
           onPageChange={setPage}
           renderItem={renderItem}/>
-        
+
         <View style={{flexDirection: 'row', alignSelf: 'center', paddingTop: 10}}>
         {
           images.map((item, idx) => {
             return (
+              (item.type == 'add' && !edit) ? null : 
               <View 
                 key={idx}
                 style={{marginHorizontal: 4, width: 8, height: 8, borderRadius: 50,
@@ -345,13 +356,19 @@ const SelfProduce = () => {
           </View>
         </View>
       </ScrollView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    margin: "8%",
+    margin: "6%",
     flex: 1,
+  },
+  modalContainer: {
+    // flex: 1,
+    height: 300,
+    backgroundColor: 'black',
   },
   profileName: {
     marginLeft: 15, 
@@ -372,15 +389,6 @@ const styles = StyleSheet.create({
     backgroundColor: colors.point,
     alignItems: 'center',
     borderRadius: 20
-  },
-  selectedTag: {
-
-  },
-  unselectedText: {
-
-  },
-  selectedText: {
-
   },
   uneditableText: {
     color: colors.gray7, 
@@ -421,6 +429,16 @@ const styles = StyleSheet.create({
   tagWrapper: {
     margin: 5, 
     flexWrap: 'wrap', 
+  },
+  selectedTag: {
+    backgroundColor: colors.point,
+    borderColor: colors.point
+  }, 
+  selectedTagText: {
+    color: colors.white
+  },
+  unselectedTagText: {
+    color: "#9E9E9E"
   }
 });
 
