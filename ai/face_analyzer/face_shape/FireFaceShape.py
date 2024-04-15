1275838e278b413075cc54ad303583ed9cd71263
Minhee <alsgml2846@gamil.com>
2024-04-15 14:00:57 +0900

    feat: add face_shape

--- ai/face_analyzer/face_shape/FireFaceShape.py
+++ ai/face_analyzer/face_shape/FireFaceShape.py
@@ -1,14 +1,49 @@
 import sys
 import os
 sys.path.append('../')
+# importing
 from PartType import PartType
 
 class FireFaceShape(PartType):
     def __init__(self):
-        self.name="역삼각형"
-        self.description=""
+        self.name="삼각형"
+        self.description="화"
 
 
-    def analyze(self, landmarks_mash, landmark_1000):
-        #비율 계산? 어떤 방식으로든 하나의 숫자로 리턴 해야함
-        return 1
+    def analyze(self, landmarks_mash,landmark_1000):
+
+        face_mide_x = landmarks_mash[152][0]
+
+        chin_list = [379,365,397,288,361,323]
+        mouth_and_chin_diff = sys.maxsize
+        for i in chin_list:
+            
+            tmp = abs(landmarks_mash[291][1] - landmarks_mash[i][1])
+            if mouth_and_chin_diff > tmp :
+                mouth_and_chin_diff = tmp
+                chin_idx = i
+        
+        print(chin_idx)
+
+        #광대 가로폭(비율 1)
+        face_width = landmarks_mash[356][0] - face_mide_x
+        #이마 가로폭
+        forehead_width = landmark_1000[40][0] - face_mide_x
+        #턱 가로폭
+        chin_width = landmarks_mash[chin_idx][0] - face_mide_x
+        
+        #광대,이마 비율
+        forehead_ratio = forehead_width / face_width
+        #광대,턱 비율
+        chin_ratio = chin_width / face_width
+
+        #이마 비율 오차
+        forehead_error = 0.851 - forehead_ratio
+        #턱 비율 오차
+        chin_error = chin_ratio - 0.832
+        
+        
+        total_error = forehead_error + chin_error 
+        
+        return total_error
+        
--- ai/face_analyzer/face_shape/WaterFaceShape.py
+++ ai/face_analyzer/face_shape/WaterFaceShape.py
@@ -6,10 +6,58 @@ from PartType import PartType
 
 class WaterFaceShape(PartType):
     def __init__(self):
-        self.name="삼각형"
-        self.description="독수리 눈은 ~~"
+        self.name="원형"
+        self.description="수"
 
 
     def analyze(self, landmarks_mash,landmark_1000):
-        #비율 계산? 어떤 방식으로든 하나의 숫자로 리턴 해야함
-        return 5
+
+        face_mide_x = landmarks_mash[152][0]
+
+        chin_list = [379,365,397,288,361,323]
+        mouth_and_chin_diff = sys.maxsize
+        for i in chin_list:
+            
+            tmp = abs(landmarks_mash[291][1] - landmarks_mash[i][1])
+            if mouth_and_chin_diff > tmp :
+                mouth_and_chin_diff = tmp
+                chin_idx = i
+
+        #광대 가로폭(비율 1)
+        face_width = landmarks_mash[356][0] - face_mide_x
+        #얼굴 세로 폭
+        face_height = landmarks_mash[152][1] - landmark_1000[71][1]
+        #이마 가로폭
+        forehead_width = landmark_1000[40][0] - face_mide_x
+        #턱 가로폭
+        chin_width = landmarks_mash[chin_idx][0] - face_mide_x
+
+
+        #광대,얼굴세로 비율
+        face_height_ratio = face_height / face_width / 2
+        #광대,이마 비율
+        forehead_ratio = forehead_width / face_width
+        #광대,턱 비율
+        chin_ratio = chin_width / face_width
+
+        #이마 비율 오차
+        if 0.851 >= face_height_ratio :
+            forehead_error = forehead_ratio / 0.851
+        else :
+            forehead_error = 0.851 / forehead_ratio 
+        #턱 비율 오차
+        if 0.832 >= chin_ratio :
+            chin_error = chin_ratio / 0.832
+        else :
+            chin_error = 0.832 / chin_ratio
+        #얼굴세로 오차
+        if 1.369 >= face_height_ratio :
+            face_height_error = face_height_ratio / 1.369
+        else :
+            face_height_error = 1.369 / face_height_ratio
+        
+        
+        total_error = forehead_error + chin_error
+        #face_height_error +
+
+        return 0.01 * total_error
