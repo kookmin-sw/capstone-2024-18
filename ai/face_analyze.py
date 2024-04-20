321ee0c4dbe2eb7ead530d9113f182526001727c
lsh9034 <lsh9034@kookmin.ac.kr>
2024-04-20 17:09:51 +0900

    feat: complete face_analyze api

--- ai/face_analyze.py
+++ ai/face_analyze.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, jsonify
 import json
 import sys
 import os
-sys.path.append('./face_analyzer')
+sys.path.append('./temp2')
 from PIL import Image
 import io
+from temp2.analyze import getType
+import os
+import onnxruntime as ort
+ort.set_default_logger_severity(3)
 
 app = Flask(__name__)
 
@@ -13,14 +17,25 @@ def home():
     return "Hello World"
 
 
-@app.route('/analyze_image', methods = ['POST']) #flask는 단일스레드+동기처리를 수행하기 때문에 face_maker는 항상 하나의 요청에서만 접근된다.
+@app.route('/analyze_face', methods = ['POST']) #flask는 단일스레드+동기처리를 수행하기 때문에 face_maker는 항상 하나의 요청에서만 접근된다.
 def generate_image():
 
+    file_path = request.values['user_id'] + '.jpeg'
+    print(request.values['user_id'])
+    print(type(request.files['image']))
+    # # 파일 저장
+    request.files['image'].save(file_path)
     print(type(request.files['image']))
     print(request.values.keys())
     
-    result = {'face':"화", "eye":'독수리'}
+    result = getType(file_path)
     
+    # 기존 사진 삭제
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    else:
+        pass
+
     return json.dumps(result, ensure_ascii=False)
 
 if __name__ == '__main__':
