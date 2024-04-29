72fff82a97efb9162435dfcc8e5bdb13e6bb4265
Minhee <alsgml2846@gamil.com>
2024-04-29 20:06:17 +0900

    fix : fix FaceShapePolicy

--- ai/face_analyzer/face_shape/FaceShapePolicy.py
+++ ai/face_analyzer/face_shape/FaceShapePolicy.py
@@ -1,9 +1,3 @@
-import face_shape.FireFaceShape
-import face_shape.GoldFaceShape
-import face_shape.GroundFaceShape
-import face_shape.TreeFaceShape
-import face_shape.WaterFaceShape
-
 def FaceShapePolicy(result):
     type_name = max(result, key=result.get)
     return type_name
