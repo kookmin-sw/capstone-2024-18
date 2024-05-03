5dd878baa570980611b4b0bce8d7c83ac23a5456
imjanghyeok <qkdrhkgn23@naver.com>
2024-05-03 09:55:13 +0900

    Feat: 채팅방이 있을 때, 상태에 따른 필요 dto class 작성

---
+++ src/main/java/capstone/facefriend/chat/service/dto/chatroom/ChatRoomMessageResponse.java
@@ -0,0 +1,32 @@
+package capstone.facefriend.chat.service.dto.chatroom;
+
+import capstone.facefriend.chat.domain.ChatMessage;
+import capstone.facefriend.chat.domain.ChatRoom;
+import capstone.facefriend.chat.domain.ChatRoomMember;
+
+import java.time.LocalDateTime;
+
+public record ChatRoomMessageResponse(
+        Long sender,
+        Long receiver,
+        ChatRoom chatRoom,
+        String content,
+        LocalDateTime sendTime,
+        String senderNickname,
+        String senderGeneratedFaceInfo,
+        String senderOriginalFaceInfo
+
+){
+    public static ChatRoomMessageResponse of(ChatRoomMember chatRoomMember, ChatMessage message) {
+        return new ChatRoomMessageResponse(
+                chatRoomMember.getSender().getId(),
+                chatRoomMember.getReceiver().getId(),
+                chatRoomMember.getChatRoom(),
+                message.getContent(),
+                message.getSendTime(),
+                message.getSender().getBasicInfo().getNickname(),
+                message.getSender().getFaceInfo().getGeneratedS3url(),
+                message.getSender().getFaceInfo().getOriginS3Url()
+        );
+    }
+}
