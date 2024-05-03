f814e3c41d509a01427c679b17b6837e4e1994bb
imjanghyeok <qkdrhkgn23@naver.com>
2024-05-03 09:17:05 +0900

    Feat: ChatRoom 도메인 작성

---
+++ src/main/java/capstone/facefriend/chat/domain/ChatRoom.java
@@ -0,0 +1,50 @@
+package capstone.facefriend.chat.domain;
+
+import capstone.facefriend.common.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.DynamicInsert;
+
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode(of = "id", callSuper = false)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Slf4j
+@DynamicInsert
+public class ChatRoom extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Status status = Status.set;
+
+    @Column
+    private boolean isPublic;
+
+    public enum Status {
+        set("Set"),
+        open("Open"),
+
+        progress("Progres"),
+        close("Close"),
+        delete("Delete");
+        private final String value;
+
+        Status(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+
+}
