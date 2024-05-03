4115ae3abafd29878dab172829c5c5d8f4b576fa
imjanghyeok <qkdrhkgn23@naver.com>
2024-05-03 10:05:58 +0900

    Feat: message 전송 service에서 쓰이는 jpa method 작성

--- src/main/java/capstone/facefriend/chat/repository/ChatMessageRepository.java
+++ src/main/java/capstone/facefriend/chat/repository/ChatMessageRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     ChatMessage findFirstByChatRoomIdOrderBySendTimeDesc(Long roomId);
+    ChatMessage save(ChatMessage chatMessage);
 
 }
--- src/main/java/capstone/facefriend/chat/repository/ChatRoomMemberRepository.java
+++ src/main/java/capstone/facefriend/chat/repository/ChatRoomMemberRepository.java
@@ -1,12 +1,23 @@
 package capstone.facefriend.chat.repository;
 
+import capstone.facefriend.chat.domain.ChatRoom;
 import capstone.facefriend.chat.domain.ChatRoomMember;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ChatRoomMemberRepository extends JpaRepository<ChatRoomMember, Long> {
+    Optional<ChatRoomMember> findByChatRoom(ChatRoom chatRoom);
+
+    @Query("SELECT c FROM ChatRoomMember c WHERE c.sender.id = :senderId AND c.receiver.id = :receiverId")
+    Optional<ChatRoomMember> findBySenderAndReceiver(@Param("senderId") Long senderId, @Param("receiverId") Long receiverId);
+
+    //    Optional<ChatRoomMember> findChatRoomMemberBySenderAAndReceiver(Member sender, Member receiver);
+    ChatRoomMember save(ChatRoomMember chatRoomMember);
+
     Optional<List<ChatRoomMember>> findAllBySenderId(Long senderId);
     Optional<List<ChatRoomMember>> findAllByReceiverId(Long senderId);
 }
