5b8ffb6e74c218b56ce41421d6804657d4f181f9
imjanghyeok <qkdrhkgn23@naver.com>
2024-05-03 10:09:03 +0900

    Feat: RedisSubscriber에서 destination으로 보내지는 dto class 작성

---
+++ src/main/java/capstone/facefriend/chat/service/dto/message/GetMessageResponse.java
@@ -0,0 +1,31 @@
+package capstone.facefriend.chat.service.dto.message;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+public class GetMessageResponse {
+    private Long roomId;
+    private Long senderId;
+    private Long receiveId;
+    private String type;
+    private String content;
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt;
+    private Boolean isRead;
+
+    public GetMessageResponse(MessageResponse messageResponse) {
+        this.roomId = messageResponse.getRoomId();
+        this.senderId = messageResponse.getSenderId();
+        this.receiveId = messageResponse.getReceiveId();
+        this.type = messageResponse.getType();
+        this.content = messageResponse.getContent();
+        this.createdAt = LocalDateTime.now();
+        this.isRead = messageResponse.getIsRead();
+    }
+}
