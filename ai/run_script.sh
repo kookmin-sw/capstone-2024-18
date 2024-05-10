fdaeb875e5851012172043175cfa688c5e5eda1d
lsh9034 <lsh9034@kookmin.ac.kr>
2024-05-11 02:08:20 +0900

    set worker to 1 & set timeout 300

--- ai/run_script.sh
+++ ai/run_script.sh
@@ -1,2 +1,2 @@
-gunicorn -w 3 --bind 0.0.0.0:5000 "face_generate:app" --daemon
-gunicorn -w 1 --bind 0.0.0.0:5001 "face_analyze:app" --daemon
+gunicorn -w 1 --bind 0.0.0.0:5000 "face_generate:app" --daemon --timeout 300
+gunicorn -w 1 --bind 0.0.0.0:5001 "face_analyze:app" --daemon --timeout 300
