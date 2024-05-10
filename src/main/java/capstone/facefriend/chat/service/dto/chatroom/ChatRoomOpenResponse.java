c84b2ff1d39b10be71addc038415e15c9cebe556
imjanghyeok <qkdrhkgn23@naver.com>
2024-05-10 13:38:09 +0900

    Refactor: Open 상태의 채팅방 Dto 수정

--- src/main/java/capstone/facefriend/chat/service/dto/chatroom/ChatRoomOpenResponse.java
+++ src/main/java/capstone/facefriend/chat/service/dto/chatroom/ChatRoomOpenResponse.java
@@ -1,14 +1,31 @@
 package capstone.facefriend.chat.service.dto.chatroom;
 
+import capstone.facefriend.chat.domain.ChatRoom;
+import capstone.facefriend.member.domain.member.Member;
+
 public record ChatRoomOpenResponse (
+        Long memberId,
+        String memberNickname,
+        String memberGeneratedS3url,
+        String memberOriginS3url,
         Long senderId,
         String senderNickname,
+        String senderGeneratedS3url,
+        String senderOriginS3url,
+        ChatRoom chatRoom,
         String message
 ) {
-    public static ChatRoomOpenResponse of(Long senderId, String senderNickname, String message) {
+    public static ChatRoomOpenResponse of(Member member, Member sender, ChatRoom chatRoom, String message) {
         return new ChatRoomOpenResponse(
-                senderId,
-                senderNickname,
+                member.getId(),
+                member.getBasicInfo().getNickname(),
+                member.getFaceInfo().getGeneratedS3url(),
+                member.getFaceInfo().getOriginS3url(),
+                sender.getId(),
+                sender.getBasicInfo().getNickname(),
+                sender.getFaceInfo().getGeneratedS3url(),
+                sender.getFaceInfo().getOriginS3url(),
+                chatRoom,
                 message
         );
     }
