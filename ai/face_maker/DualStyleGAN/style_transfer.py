dfc39ac048b86501f18a751ddbd32b1914ea1070
lsh9034 <lsh9034@kookmin.ac.kr>
2024-05-11 00:17:52 +0900

    refactor:remove main

--- ai/face_maker/DualStyleGAN/style_transfer.py
+++ ai/face_maker/DualStyleGAN/style_transfer.py
@@ -167,102 +167,102 @@ class StyleTransfer():
         print('Generate images successfully!')
         for i in range(len(viz)):
             print(viz[i].shape)
-        save_name = args.name+'_%d_%s'%(args.style_id, os.path.basename(args.content).split('.')[0])
+        #save_name = args.name+'_%d_%s'%(args.style_id, os.path.basename(args.content).split('.')[0])
         #save_image(torchvision.utils.make_grid(F.adaptive_avg_pool2d(torch.cat(viz, dim=0), 256), 4, 2).cpu(), 
         #os.path.join(args.output_path, save_name+'_overview.jpg'))
         #save_image(img_gen[0].cpu(), os.path.join(args.output_path, save_name+'.jpg'))
         face = ((img_gen[0].detach().numpy().transpose(1, 2, 0) + 1.0) * 127.5).astype(np.uint8)
         return face
 
-if __name__ == "__main__":
-    device = "cpu"
+# if __name__ == "__main__":
+#     device = "cpu"
 
-    parser = TestOptions()
-    args = parser.parse()
-    print('*'*98)
+#     parser = TestOptions()
+#     args = parser.parse()
+#     print('*'*98)
     
-    transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5],std=[0.5,0.5,0.5]),
-    ])
+#     transform = transforms.Compose([
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.5, 0.5, 0.5],std=[0.5,0.5,0.5]),
+#     ])
     
-    generator = DualStyleGAN(1024, 512, 8, 2, res_index=6)
-    generator.eval()
+#     generator = DualStyleGAN(1024, 512, 8, 2, res_index=6)
+#     generator.eval()
 
-    ckpt = torch.load(os.path.join(args.model_path, args.style, args.model_name), map_location=lambda storage, loc: storage)
-    generator.load_state_dict(ckpt["g_ema"])
-    generator = generator.to(device)
+#     ckpt = torch.load(os.path.join(args.model_path, args.style, args.model_name), map_location=lambda storage, loc: storage)
+#     generator.load_state_dict(ckpt["g_ema"])
+#     generator = generator.to(device)
     
-    if args.wplus:
-        model_path = os.path.join(args.model_path, 'encoder_wplus.pt')
-    else:
-        model_path = os.path.join(args.model_path, 'encoder.pt')
-    ckpt = torch.load(model_path, map_location='cpu')
-    opts = ckpt['opts']
-    opts['checkpoint_path'] = model_path
-    if 'output_size' not in opts:
-        opts['output_size'] = 1024    
-    opts = Namespace(**opts)
-    opts.device = device
-    encoder = pSp(opts)
-    encoder.eval()
-    encoder.to(device)
+#     if args.wplus:
+#         model_path = os.path.join(args.model_path, 'encoder_wplus.pt')
+#     else:
+#         model_path = os.path.join(args.model_path, 'encoder.pt')
+#     ckpt = torch.load(model_path, map_location='cpu')
+#     opts = ckpt['opts']
+#     opts['checkpoint_path'] = model_path
+#     if 'output_size' not in opts:
+#         opts['output_size'] = 1024    
+#     opts = Namespace(**opts)
+#     opts.device = device
+#     encoder = pSp(opts)
+#     encoder.eval()
+#     encoder.to(device)
 
-    exstyles = np.load(os.path.join(args.model_path, args.style, args.exstyle_name), allow_pickle='TRUE').item()
+#     exstyles = np.load(os.path.join(args.model_path, args.style, args.exstyle_name), allow_pickle='TRUE').item()
 
-    z_plus_latent=not args.wplus
-    return_z_plus_latent=not args.wplus
-    input_is_latent=args.wplus    
+#     z_plus_latent=not args.wplus
+#     return_z_plus_latent=not args.wplus
+#     input_is_latent=args.wplus    
     
-    print('Load models successfully!')
+#     print('Load models successfully!')
     
-    with torch.no_grad():
-        viz = []
-        # load content image
-        if args.align_face:
-            I = transform(run_alignment(args)).unsqueeze(dim=0).to(device)
-            I = F.adaptive_avg_pool2d(I, 1024)
-        else:
-            I = load_image(args.content).to(device)
-        viz += [I]
+#     with torch.no_grad():
+#         viz = []
+#         # load content image
+#         if args.align_face:
+#             I = transform(run_alignment(args)).unsqueeze(dim=0).to(device)
+#             I = F.adaptive_avg_pool2d(I, 1024)
+#         else:
+#             I = load_image(args.content).to(device)
+#         viz += [I]
 
-        # reconstructed content image and its intrinsic style code
-        img_rec, instyle = encoder(F.adaptive_avg_pool2d(I, 256), randomize_noise=False, return_latents=True, 
-                                   z_plus_latent=z_plus_latent, return_z_plus_latent=return_z_plus_latent, resize=False)  
-        img_rec = torch.clamp(img_rec.detach(), -1, 1)
-        viz += [img_rec]
+#         # reconstructed content image and its intrinsic style code
+#         img_rec, instyle = encoder(F.adaptive_avg_pool2d(I, 256), randomize_noise=False, return_latents=True, 
+#                                    z_plus_latent=z_plus_latent, return_z_plus_latent=return_z_plus_latent, resize=False)  
+#         img_rec = torch.clamp(img_rec.detach(), -1, 1)
+#         viz += [img_rec]
 
-        stylename = list(exstyles.keys())[args.style_id]
-        latent = torch.tensor(exstyles[stylename]).to(device)
-        if args.preserve_color and not args.wplus:
-            latent[:,7:18] = instyle[:,7:18]
-        # extrinsic styte code
-        exstyle = generator.generator.style(latent.reshape(latent.shape[0]*latent.shape[1], latent.shape[2])).reshape(latent.shape)
-        if args.preserve_color and args.wplus:
-            exstyle[:,7:18] = instyle[:,7:18]
+#         stylename = list(exstyles.keys())[args.style_id]
+#         latent = torch.tensor(exstyles[stylename]).to(device)
+#         if args.preserve_color and not args.wplus:
+#             latent[:,7:18] = instyle[:,7:18]
+#         # extrinsic styte code
+#         exstyle = generator.generator.style(latent.reshape(latent.shape[0]*latent.shape[1], latent.shape[2])).reshape(latent.shape)
+#         if args.preserve_color and args.wplus:
+#             exstyle[:,7:18] = instyle[:,7:18]
             
-        # load style image if it exists
-        S = None
-        if os.path.exists(os.path.join(args.data_path, args.style, 'images/train', stylename)):
-            S = load_image(os.path.join(args.data_path, args.style, 'images/train', stylename)).to(device)
-            viz += [S]
+#         # load style image if it exists
+#         S = None
+#         if os.path.exists(os.path.join(args.data_path, args.style, 'images/train', stylename)):
+#             S = load_image(os.path.join(args.data_path, args.style, 'images/train', stylename)).to(device)
+#             viz += [S]
 
-        # style transfer 
-        # input_is_latent: instyle is not in W space
-        # z_plus_latent: instyle is in Z+ space
-        # use_res: use extrinsic style path, or the style is not transferred
-        # interp_weights: weight vector for style combination of two paths
-        img_gen, _ = generator([instyle], exstyle, input_is_latent=input_is_latent, z_plus_latent=z_plus_latent,
-                              truncation=args.truncation, truncation_latent=0, use_res=True, interp_weights=args.weight)
-        img_gen = torch.clamp(img_gen.detach(), -1, 1)
-        viz += [img_gen]
+#         # style transfer 
+#         # input_is_latent: instyle is not in W space
+#         # z_plus_latent: instyle is in Z+ space
+#         # use_res: use extrinsic style path, or the style is not transferred
+#         # interp_weights: weight vector for style combination of two paths
+#         img_gen, _ = generator([instyle], exstyle, input_is_latent=input_is_latent, z_plus_latent=z_plus_latent,
+#                               truncation=args.truncation, truncation_latent=0, use_res=True, interp_weights=args.weight)
+#         img_gen = torch.clamp(img_gen.detach(), -1, 1)
+#         viz += [img_gen]
 
-    print('Generate images successfully!')
-    for i in range(len(viz)):
-        print(viz[i].shape)
-    save_name = args.name+'_%d_%s'%(args.style_id, os.path.basename(args.content).split('.')[0])
-    save_image(torchvision.utils.make_grid(F.adaptive_avg_pool2d(torch.cat(viz, dim=0), 256), 4, 2).cpu(), 
-               os.path.join(args.output_path, save_name+'_overview.jpg'))
-    save_image(img_gen[0].cpu(), os.path.join(args.output_path, save_name+'.jpg'))
+#     print('Generate images successfully!')
+#     for i in range(len(viz)):
+#         print(viz[i].shape)
+#     save_name = args.name+'_%d_%s'%(args.style_id, os.path.basename(args.content).split('.')[0])
+#     save_image(torchvision.utils.make_grid(F.adaptive_avg_pool2d(torch.cat(viz, dim=0), 256), 4, 2).cpu(), 
+#                os.path.join(args.output_path, save_name+'_overview.jpg'))
+#     save_image(img_gen[0].cpu(), os.path.join(args.output_path, save_name+'.jpg'))
 
-    print('Save images successfully!')
+#     print('Save images successfully!')
