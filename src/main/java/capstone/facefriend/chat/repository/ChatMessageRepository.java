d2fd15bbd2304d5b5a5f83943fcac6ddead157f0
imjanghyeok <qkdrhkgn23@naver.com>
2024-05-10 13:44:15 +0900

    Refactor: 작성한 서비스 코드에 맞게 ChatMessageRepository 코드 수정

--- src/main/java/capstone/facefriend/chat/repository/ChatMessageRepository.java
+++ src/main/java/capstone/facefriend/chat/repository/ChatMessageRepository.java
@@ -1,11 +1,15 @@
 package capstone.facefriend.chat.repository;
 
 import capstone.facefriend.chat.domain.ChatMessage;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    ChatMessage findFirstByChatRoomIdOrderBySendTimeDesc(Long roomId);
     ChatMessage save(ChatMessage chatMessage);
-
+    List<ChatMessage> findChatMessagesByChatRoom_IdAndSendTimeBefore(Long roomId, LocalDateTime time, Pageable pageable);
+    List<ChatMessage> findChatMessagesByChatRoomId(Long roomId);
 }
