55bfed2412a2da49a27b8ef0ff993db31f8dba7a
imjanghyeok <qkdrhkgn23@naver.com>
2024-05-10 13:57:45 +0900

    Refactor: created_at을 sendtime으로 변경

--- src/main/java/capstone/facefriend/chat/service/dto/message/GetMessageResponse.java
+++ src/main/java/capstone/facefriend/chat/service/dto/message/GetMessageResponse.java
@@ -14,10 +14,11 @@ public class GetMessageResponse {
     private Long senderId;
     private Long receiveId;
     private String senderNickname;
+    private String senderFaceInfoS3Url;
     private String type;
     private String content;
     @JsonFormat(pattern = "yyyy-MM-dd hh:mm", timezone = "Asia/Seoul")
-    private LocalDateTime createdAt;
+    private LocalDateTime sendTime;
     private Boolean isRead;
 
     public GetMessageResponse(MessageResponse messageResponse) {
@@ -25,9 +26,10 @@ public class GetMessageResponse {
         this.senderId = messageResponse.getSenderId();
         this.receiveId = messageResponse.getReceiveId();
         this.senderNickname = messageResponse.getSenderNickname();
+        this.senderFaceInfoS3Url = messageResponse.getSenderFaceInfoS3Url();
         this.type = messageResponse.getType();
         this.content = messageResponse.getContent();
-        this.createdAt = LocalDateTime.now();
+        this.sendTime = messageResponse.getCreatedAt();
         this.isRead = messageResponse.getIsRead();
     }
 }
