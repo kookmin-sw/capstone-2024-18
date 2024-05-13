7c2249c3028519c12072ded8bb8ced821888982f
lsh9034 <lsh9034@kookmin.ac.kr>
2024-05-13 18:14:14 +0900

    feat: worker 3 & set timeout 300

--- ai/run_script.sh
+++ ai/run_script.sh
@@ -1,2 +1,2 @@
-gunicorn -w 2 --bind 0.0.0.0:5000 "face_generate:app" --daemon --enable-stdio-inheritance --log-level debug
-gunicorn -w 1 --bind 0.0.0.0:5001 "face_analyze:app" --daemon
+gunicorn -w 3 --bind 0.0.0.0:5000 "face_generate:app" --daemon --enable-stdio-inheritance --log-level debug --timeout 300
+gunicorn -w 1 --bind 0.0.0.0:5001 "face_analyze:app" --daemon --timeout 300
