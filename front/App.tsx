33dbe46b2840099529739baa7b1dded1c67888c8
nanana3679 <nanana3679@gmail.com>
2024-05-13 10:39:16 +0900

    feat: ChatRoomProvider 적용

--- front/App.tsx
+++ front/App.tsx
@@ -16,34 +16,37 @@ import NicknamePage from './pages/NicknamePage.tsx';
 import BasicInfoWithoutNickname from './pages/BasicInfoWithoutNickname.tsx';
 import ChatContextProvider from './store/chat-context.tsx';
 import UserContextProvider from './store/user-context.tsx';
-import ChatPage from './components/chat/ChatPage.tsx';
+import ChatRoomContextProvider from './store/chat-room-context.tsx';
+import ChatRoomList from './components/chat/ChatRoomList.tsx';
 
 const Stack = createStackNavigator();
 function App() {
   return (
     <AuthContextProvider>
       <ChatContextProvider>
-        <UserContextProvider>
-          <SafeAreaProvider>
-            <NavigationContainer>
-              <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
-                <Stack.Screen name="Login" component={Login}/>
-                <Stack.Screen name="FindEmail" component={FindEmail}/>
-                <Stack.Screen name="FindPw" component={FindPw}/>
-                <Stack.Screen name="Signup" component={Signup}/>
-                <Stack.Screen name='Main' component={BottomNavigationPage}/>
-                <Stack.Screen name='Home' component={Home}/>
-                <Stack.Screen name='BasicInfo' component={BasicInfoPage}/>
-                <Stack.Screen name='FaceInfo' component={FaceInfoPage}/>
-                <Stack.Screen name='FaceFeature' component={FaceFeaturePage}/>
-                <Stack.Screen name='Nickname' component={NicknamePage}/>
-                <Stack.Screen name='Chat' component={ChatPage}/>
-                <Stack.Screen name='BasicInfoWithoutNickname' component={BasicInfoWithoutNickname}/>
-                {/* Bottom Navigation이 있는 페이지의 경우 SafeAreaView를 이용하면 ios에서 bottomNavigation이 제대로 안 보임 */}
-              </Stack.Navigator>
-            </NavigationContainer>
-          </SafeAreaProvider>  
-        </UserContextProvider>
+        <ChatRoomContextProvider>
+          <UserContextProvider>
+            <SafeAreaProvider>
+              <NavigationContainer>
+                <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
+                  <Stack.Screen name="Login" component={Login}/>
+                  <Stack.Screen name="FindEmail" component={FindEmail}/>
+                  <Stack.Screen name="FindPw" component={FindPw}/>
+                  <Stack.Screen name="Signup" component={Signup}/>
+                  <Stack.Screen name='Main' component={BottomNavigationPage}/>
+                  <Stack.Screen name='Home' component={Home}/>
+                  <Stack.Screen name='BasicInfo' component={BasicInfoPage}/>
+                  <Stack.Screen name='FaceInfo' component={FaceInfoPage}/>
+                  <Stack.Screen name='FaceFeature' component={FaceFeaturePage}/>
+                  <Stack.Screen name='Nickname' component={NicknamePage}/>
+                  <Stack.Screen name='BasicInfoWithoutNickname' component={BasicInfoWithoutNickname}/>
+                  <Stack.Screen name='Chat' component={ChatRoomList}/>
+                  {/* Bottom Navigation이 있는 페이지의 경우 SafeAreaView를 이용하면 ios에서 bottomNavigation이 제대로 안 보임 */}
+                </Stack.Navigator>
+              </NavigationContainer>
+            </SafeAreaProvider>  
+          </UserContextProvider>
+        </ChatRoomContextProvider>
       </ChatContextProvider>
     </AuthContextProvider>
   );
