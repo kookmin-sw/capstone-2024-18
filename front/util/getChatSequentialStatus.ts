029267fffcf55a14c6a193ce7be990b2038869bd
nanana3679 <nanana3679@gmail.com>
2024-05-14 18:53:31 +0900

    fix: 채팅 serial 조건 수정

--- front/util/getChatSequentialStatus.ts
+++ front/util/getChatSequentialStatus.ts
@@ -7,11 +7,11 @@ export const getIsDailyInitial = (prevChat: ChatProps | undefined, chat: ChatPro
 }
 
 export const getIsInitial = (prevChat: ChatProps | undefined, chat: ChatProps | undefined) => {
-  const isInitial = prevChat === undefined || !areMinutesEqual(prevChat.sendTime, chat?.sendTime) || prevChat?.id !== chat?.id;
+  const isInitial = prevChat === undefined || !areMinutesEqual(prevChat.sendTime, chat?.sendTime) || prevChat?.id === chat?.id;
   return isInitial;
 }
 
 export const getIsFinal = (chat: ChatProps | undefined, nextChat: ChatProps | undefined) => {
-  const isFinal = nextChat === undefined || !areMinutesEqual(nextChat.sendTime, chat?.sendTime) || nextChat?.id !== chat?.id;
+  const isFinal = nextChat === undefined || !areMinutesEqual(nextChat.sendTime, chat?.sendTime) || nextChat?.id === chat?.id;
   return isFinal;
 }
