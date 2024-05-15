da04866907e34640334eab20a921256f9b6b72d2
imjanghyeok <qkdrhkgn23@naver.com>
2024-05-15 11:06:35 +0900

    Refactor: 프론트에서 구분할 수 있게 메서드 추가하여 채팅 요청 응답 dto 수정

--- src/main/java/capstone/facefriend/chat/service/dto/heart/HeartReplyResponse.java
+++ src/main/java/capstone/facefriend/chat/service/dto/heart/HeartReplyResponse.java
@@ -1,11 +1,14 @@
 package capstone.facefriend.chat.service.dto.heart;
 
 public record HeartReplyResponse(
+        String method,
         Long senderId,
         String intention
+
 ) {
-    public static HeartReplyResponse of (Long senderId, HeartReplyRequest heartReplyRequest) {
+    public static HeartReplyResponse of (Long senderId, HeartReplyRequest heartReplyRequest, String method) {
         return new HeartReplyResponse(
+                method,
                 senderId,
                 heartReplyRequest.intention()
         );
