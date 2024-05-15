703d1bcab03e7aa93acb1e3110336a4f3a4ce5e0
imjanghyeok <qkdrhkgn23@naver.com>
2024-05-15 11:04:44 +0900

    Refactor: 프론트에서 구분할 수 있게 메서드 추가하여 채팅방 나가기 dto 수정

--- src/main/java/capstone/facefriend/chat/service/dto/chatroom/ChatRoomLeftResponse.java
+++ src/main/java/capstone/facefriend/chat/service/dto/chatroom/ChatRoomLeftResponse.java
@@ -2,15 +2,23 @@ package capstone.facefriend.chat.service.dto.chatroom;
 
 import capstone.facefriend.member.domain.member.Member;
 
+import java.time.LocalDateTime;
+
 public record ChatRoomLeftResponse (
+        String method,
+        Long roomId,
         Long senderId,
         String senderNickname,
+        LocalDateTime sendTime,
         String content
 ){
-    public static ChatRoomLeftResponse of (Member sender, String content) {
+    public static ChatRoomLeftResponse of (String method, Long roomId, Member sender, LocalDateTime sendTime, String content) {
     return new ChatRoomLeftResponse(
+            method,
+            roomId,
             sender.getId(),
             sender.getBasicInfo().getNickname(),
+            sendTime,
             content
     );
     }
