3816ad6843f272a377ba17a4b2fbf70054e55817
imjanghyeok <qkdrhkgn23@naver.com>
2024-05-15 11:06:28 +0900

    Refactor: 프론트에서 구분할 수 있게 메서드 추가하여 채팅 요청 보내기 dto 수정

--- src/main/java/capstone/facefriend/chat/service/dto/heart/GetSendHeartResponse.java
+++ src/main/java/capstone/facefriend/chat/service/dto/heart/GetSendHeartResponse.java
@@ -1,5 +1,6 @@
 package capstone.facefriend.chat.service.dto.heart;
 
+import capstone.facefriend.chat.domain.ChatRoom;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,21 +11,29 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 public class GetSendHeartResponse {
+    private String method;
     private String senderName;
     private Long senderId;
-    private Long receiveId;
+    private Long memberId;
     private String type;
-    private Long roomId;
+    private String senderGeneratedS3url;
+    private String senderOriginS3url;
+    private ChatRoom chatRoom;
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime sendTime;
+    private Boolean isSender;
 
 
     public GetSendHeartResponse(SendHeartResponse sendHeartResponse) {
+        this.method = sendHeartResponse.getMethod();
         this.senderName = sendHeartResponse.getSenderName();
         this.senderId = sendHeartResponse.getSenderId();
-        this.receiveId = sendHeartResponse.getReceiveId();
+        this.memberId = sendHeartResponse.getMemberId();
         this.type = sendHeartResponse.getType();
-        this.roomId = sendHeartResponse.getRoomId();
         this.sendTime = sendHeartResponse.getCreatedAt();
+        this.isSender = sendHeartResponse.isSender();
+        this.chatRoom = sendHeartResponse.getChatRoom();
+        this.senderGeneratedS3url = sendHeartResponse.getSenderGeneratedS3url();
+        this.senderOriginS3url = sendHeartResponse.getSenderOriginS3url();
     }
 }
