c484bef7563fe7fad6839c9932b9892c1eca161a
nanana3679 <nanana3679@gmail.com>
2024-05-19 20:26:18 +0900

    fix: 채팅방 이동 안되는 버그 수정

--- front/components/chat/ChatRoomList.tsx
+++ front/components/chat/ChatRoomList.tsx
@@ -5,14 +5,14 @@ import HeaderBar from '../HeaderBar';
 import ChatPage from './ChatPage';
 import { ChatRoomContext } from '../../store/chat-room-context';
 import { formatTimeDifference } from '../../util/formatTime';
-import { ChatContext } from '../../store/chat-context';
 import { AuthContext } from '../../store/auth-context';
 import { Icon, IconButton } from 'react-native-paper';
 import CustomButton from '../CustomButton';
 
 const ChatRoomList = ({navigation}: any) => {
+  const authCtx = useContext(AuthContext);
   const chatRoomCtx = useContext(ChatRoomContext);
-  const chatCtx = useContext(ChatContext);
+  
   const [roomId, setRoomId] = useState(0);
 
   const handleOnPress = (roomId: number) => {
@@ -25,61 +25,64 @@ const ChatRoomList = ({navigation}: any) => {
 
   useEffect(() => {
     if (roomId) return;
+    console.log(roomId);
     chatRoomCtx.getChatRoomList();
   }, [roomId])
 
-  const authCtx = useContext(AuthContext);
+  const defaultContent = 
+  <View style={[styles.container, {paddingHorizontal: 32}]}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Icon size={150} source={require('../../assets/images/surprise.png')}/>
+      <View style={{marginTop: 20, alignItems: 'center'}}>
+        <Text style={styles.hintText}>채팅방이 존재하지 않습니다. {'\n'}새로운 친구를 찾아보세요!</Text>
+      </View>
+    </View>
+    <View style={styles.bottomContainer}>
+      <CustomButton
+        containerStyle={{backgroundColor: colors.point, elevation: 4}}
+        onPress={()=>{navigation.navigate('sub1')}}
+        textStyle={{color: colors.white, fontSize: 18, fontFamily: 'Pretendard-SemiBold', letterSpacing: -18* 0.02}}>새로운 친구 찾으러 가기
+      </CustomButton>
+    </View>
+  </View>
+
+  const chatRoomList =
+  <ScrollView style={{ padding: 20 }}>
+    {Object.values(chatRoomCtx.chatRooms).map((chatRoom) => (
+    <Pressable key={chatRoom.roomId} onPress={handleOnPress.bind(this, chatRoom.roomId)}>
+      <Text style={{color:'black'}}>roomId: {chatRoom.roomId} senderId:{chatRoom.senderId}</Text>
+      <View style={styles.listItemContainer}>
+        {!chatRoom.senderGeneratedS3url && <View style={styles.profile}/>}
+        {chatRoom.senderGeneratedS3url && <Image
+            source={{ uri: chatRoom.senderGeneratedS3url }}
+            style={[styles.profile, { backgroundColor: colors.white }]}
+          />}
+        <View style={{ flex: 1 }}>
+          <Text style={styles.nickname}>{chatRoom.senderNickname}</Text>
+          <Text style={styles.chatContent}>{chatRoom.content ? chatRoom.content : "대화 내용이 없습니다."}</Text>
+        </View>
+        <View style={styles.listItemRightContainer}>
+          <View style={{ flex: 1 }}>
+            {chatRoom.content === '하트를 받았습니다.' && 
+            <View style={{ flexDirection: 'row' }}>
+              <View><IconButton icon='close' style={styles.rejectButtonContainer} size={24} iconColor='#FFFFFFCC'/></View>
+              <View><IconButton icon='check' style={styles.acceptButtonContainer} size={24} iconColor='#FFFFFFCC'/></View>
+            </View>}
+          </View>
+          <Text style={styles.elapsedTimeText}>{formatTimeDifference(chatRoom.updatedAt)}</Text>
+        </View>
+      </View>
+    </Pressable>)
+    )}
+  </ScrollView>
 
   return (
     <View style={styles.container}>
-    {chatRoomCtx.chatRooms ? <>
+    {!roomId && <>
       <HeaderBar onPress={() => {}}>다이렉트 메세지</HeaderBar>
       <Text style={{color:'black'}}>myUserId: {authCtx.userId}</Text>
-      <ScrollView style={{ padding: 20 }}>
-        {Object.values(chatRoomCtx.chatRooms).map((chatRoom) => (
-        <Pressable key={chatRoom.roomId} onPress={handleOnPress.bind(this, chatRoom.roomId)}>
-          <Text style={{color:'black'}}>roomId: {chatRoom.roomId} senderId:{chatRoom.senderId}</Text>
-          <View style={styles.listItemContainer}>
-            {!chatRoom.senderGeneratedS3url && <View style={styles.profile}/>}
-            {chatRoom.senderGeneratedS3url && <Image
-                source={{ uri: chatRoom.senderGeneratedS3url }}
-                style={[styles.profile, { backgroundColor: colors.white }]}
-              />}
-            <View style={{ flex: 1 }}>
-              <Text style={styles.nickname}>{chatRoom.senderNickname}</Text>
-              <Text style={styles.chatContent}>{chatRoom.content ? chatRoom.content : "대화 내용이 없습니다."}</Text>
-            </View>
-            <View style={styles.listItemRightContainer}>
-              <View style={{ flex: 1 }}>
-                {chatRoom.content === '하트를 받았습니다.' && 
-                <View style={{ flexDirection: 'row' }}>
-                  <View><IconButton icon='close' style={styles.rejectButtonContainer} size={24} iconColor='#FFFFFFCC'/></View>
-                  <View><IconButton icon='check' style={styles.acceptButtonContainer} size={24} iconColor='#FFFFFFCC'/></View>
-                </View>}
-              </View>
-              <Text style={styles.elapsedTimeText}>{formatTimeDifference(chatRoom.updatedAt)}</Text>
-            </View>
-          </View>
-        </Pressable>)
-        )}
-      </ScrollView>
-    </>:
-    <View style={[styles.container, {paddingHorizontal: 32}]}>
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Icon size={150} source={require('../../assets/images/surprise.png')}/>
-        <View style={{marginTop: 20, alignItems: 'center'}}>
-          <Text style={styles.hintText}>채팅방이 존재하지 않습니다. {'\n'}새로운 친구를 찾아보세요!</Text>
-        </View>
-      </View>
-      <View style={styles.bottomContainer}>
-        <CustomButton
-          containerStyle={{backgroundColor: colors.point, elevation: 4}}
-          onPress={()=>{navigation.navigate('sub1')}}
-          textStyle={{color: colors.white, fontSize: 18, fontFamily: 'Pretendard-SemiBold', letterSpacing: -18* 0.02}}>새로운 친구 찾으러 가기
-        </CustomButton>
-      </View>
-    </View>
-    }
+      {chatRoomCtx.chatRooms ? chatRoomList : defaultContent}
+    </>}
     {!!roomId && <ChatPage onBack={handleOnBack} roomId={roomId}/>}
     </View>
   )
