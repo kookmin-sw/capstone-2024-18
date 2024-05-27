a29667ab94a38d9306ef59189ed8554d43d66b4c
Minji Yun <gee2450@kookmin.ac.kr>
2024-05-27 17:52:42 +0900

    Feat: github page를 위한 first commit

---
+++ capstone-2024-18/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header.tsx';
+import Contents from './component/Contents.tsx';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Contents/>
+    </>
+  );
+}
+
+export default App;
---
+++ capstone-2024-18/src/component/Contents.tsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+function Contents() {
+  return (
+    <div id="container">
+      
+    </div>
+  );
+}
+
+export default Contents;
