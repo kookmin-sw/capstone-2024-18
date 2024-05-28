0b69ffab6df53873a7e3007d17b2c5dcbd041d11
Minji Yun <gee2450@kookmin.ac.kr>
2024-05-28 13:44:31 +0900

    Feat: Desktop 배경 오류나는 거 수정 - 이미지 변경 - 배경 깨지는 오류 수정

--- capstone-2024-18/src/Components/Desktop/Container.jsx
+++ capstone-2024-18/src/Components/Desktop/Container.jsx
@@ -1,12 +1,12 @@
 import classes from './DesktopPage.module.css';
 
-function Container({height}) {
+function Container() {
   const { innerWidth: width } = window;
 
   return (
-    <div style={{flexDirection: 'column', display: 'flex', height: height}}>
-      <img src={require('../../images/halfCircle.png')} width={width} height={width} style={{margin: 0, position: 'unset'}}/>
-      <div style={{backgroundColor: '#FFECEB', flex: 1}}/>
+    <div style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
+      <img src={`${process.env.PUBLIC_URL}/halfCircle.png`} width={width} height={width} style={{margin: 0, position: 'unset', backgroundColor: '#fff'}}/>
+      <div style={{backgroundColor: '#FFECEB', width: width, flex: 1}}/>
     </div>
   );
 }
--- capstone-2024-18/src/Components/Desktop/TeamMember.jsx
+++ capstone-2024-18/src/Components/Desktop/TeamMember.jsx
@@ -1,10 +1,12 @@
+import classes from './DesktopPage.module.css';
+
 function TeamMember({name, image, contents}) {
     return (
       <div style={{borderRadius: 10, padding: 10,marginBottom: 20, backgroundColor: '#FFF8F7', flexDirection: 'row', display: 'flex', borderStyle: 'solid', borderWidth: 0.3, borderColor: '#C6302B'}}>
         <img src={image} width={100} height={100} style={{position: 'relative', margin: 0}}/>
         <div style={{marginLeft: 20, alignSelf: 'center'}}>
-          <text>{name}</text>
-          <div dangerouslySetInnerHTML={{__html: contents}}/>
+          <text id={classes.contentSubTitle} style={{display: 'block', marginBottom: 10}}>{name}</text>
+          <div id={classes.memberText} dangerouslySetInnerHTML={{__html: contents}}/>
         </div>
       </div>
     );
