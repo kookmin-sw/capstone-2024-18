9894dd58cfe225ecdde629319023b97d31154865
Minji Yun <gee2450@kookmin.ac.kr>
2024-05-28 19:56:39 +0900

    Feat: mobile url 수정

--- capstone-2024-18/src/Components/Mobile/Intro.jsx
+++ capstone-2024-18/src/Components/Mobile/Intro.jsx
@@ -6,7 +6,7 @@ const Intro = () => {
       <img className={classes.logo} src={`${process.env.PUBLIC_URL}/facefriend-logo.png`} alt="GitHub Logo"/>
       <p className={classes.intro}>
         <span className='bold' style={{ fontSize: "1.75rem" }}>인간관계를 맺는 색다른 즐거움, <span className='primary'>FACE FRIEND</span></span><br/>
-        <a href='https://drive.google.com/file/d/1kkJTefS8EY83Nda040PhReRYd5413jDo/view?usp=drive_link'>
+        <a href='https://drive.google.com/file/d/1VuFRnFYh9nLRFrohUMbWwr6bjLkoaPlZ/view?usp=sharing'>
           <span className='underline'>Android APK 파일 다운로드 링크 😎</span>
         </a>
       </p>
