371bfc5331551ab076b248cb2d8e3a8532a86ef5
Minji Yun <gee2450@kookmin.ac.kr>
2024-05-30 18:59:47 +0900

    Feat: Desktop style 변경

--- capstone-2024-18/src/Components/Desktop/Contents.jsx
+++ capstone-2024-18/src/Components/Desktop/Contents.jsx
@@ -38,7 +38,7 @@ function Contents({onHeightChange}) {
     <div ref={childRef} id={classes.container}>
       <div style={{flexDirection: 'row', display: 'flex', marginBottom: 90}}>
         <img src={`${process.env.PUBLIC_URL}/screen1.png`} width={"25%"} style={{position: 'relative', margin: 0}}/>
-        <div style={{paddingTop: 50, paddingLeft: '6%', paddingRight: '6%'}}>
+        <div style={{paddingTop: 20, paddingLeft: '6%', paddingRight: '6%'}}>
           <text id={classes.contentTitle} style={{display: 'block', marginBottom: 50}}>1. 프로젝트 소개</text>
           <div id={classes.contentSubTitle} dangerouslySetInnerHTML={{__html: `<span style='color: ${point_color};'>페이스 프렌드(FaceFriend)</span>는 기존의 데이팅 앱에서 사용자가 느꼈던 불편한 점들을 해결하기 위한 관상 기반 앱 서비스입니다. <br><br><span style='color: ${point_color};'>얼굴 노출의 부담, 얼굴 이미지에 대한 의존</span>과 같은 문제를 해결하기 위해 페이스 프렌드는 <span style='color: ${point_color};'>5가지 기능</span>을 제공합니다.`}}/>
         </div>
@@ -46,52 +46,57 @@ function Contents({onHeightChange}) {
       
       <text id={classes.contentTitle} style={{display: 'block'}}>2. 기능 소개</text>
       <div style={{marginBottom: 90}}>
-        <div style={{flexDirection:'row', display: 'flex'}}>
+        <div style={{flexDirection:'row', display: 'flex', marginBottom: 50}}>
           <FunctionText title={"1. AI 관상 이미지"} contents={`얼굴 이미지를 업로드하고 자신과 비슷한 스타일 이미지를 선택하여 <span style='color: ${point_color};'>관상 이미지</span>를 생성합니다. 관상 이미지는 사용자의 얼굴 특징을 담아내어 실제 얼굴 이미지 대신 사용할 수 있습니다. <br><br>사용자는 상대의 관상 이미지를 보며 상대의 실제 얼굴을 유추하는 즐거움을 느낄 수 있습니다.`}/>
-          <img src={`${process.env.PUBLIC_URL}/screen2.png`} width={"25%"} style={{position: 'relative', margin: 0, marginLeft: '6%', marginRight: '6%'}}/>
+          <view style={{width: 20}}/>
+          <img src={`${process.env.PUBLIC_URL}/screen2.png`} width={"25%"} style={{position: 'relative', margin: 0}}/>
         </div>
-        <div style={{flexDirection:'row', display: 'flex'}}>
-          <img src={`${process.env.PUBLIC_URL}/screen3.png`} width={"25%"} style={{position: 'relative', margin: 0, marginLeft: '6%', marginRight: '6%'}}/>
+        <div style={{flexDirection:'row', display: 'flex', marginBottom: 50}}>
+          <img src={`${process.env.PUBLIC_URL}/screen3.png`} width={"25%"} style={{position: 'relative', margin: 0}}/>
+          <view style={{width: 20}}/>
           <FunctionText title={"2. AI 관상 분석"} contents={`얼굴 이미지를 업로드하여 관상을 분석합니다. <span style='color: ${point_color};'>눈썹, 눈, 코, 입, 얼굴형</span>에 대해 분석하고 <span style='color: ${point_color};'>관상학의 오행형(五行形)</span>에 따라 사용자는 <span style='color: ${point_color};'>화/수/목/금/토</span> 중 하나로 분류됩니다. <br><br> 사용자는 상대의 관상 분석을 보며 상대의 실제 성격을 유추하는 즐거움을 느낄 수 있습니다.`}/>
         </div>
-        <div style={{flexDirection:'row', display: 'flex'}}>
+        <div style={{flexDirection:'row', display: 'flex', marginBottom: 50}}>
           <FunctionText title={"3. AI 관상 궁합 추천"} contents={`AI 관상 분석의 결과로 사용자는 오행형이 분류됩니다. <span style='color: ${point_color};'>관상 궁합이 좋은 오행형</span>을 가진 다른 사용자들을 추천합니다. <br><br> 관상 궁합 추천 이외에도 관심분야별 추천 기능도 존재합니다.`}/>
-          <img src={`${process.env.PUBLIC_URL}/screen4.png`} width={"25%"} style={{position: 'relative', margin: 0, marginLeft: '6%', marginRight: '6%'}}/>
+          <view style={{width: 20}}/>
+          <img src={`${process.env.PUBLIC_URL}/screen4.png`} width={"25%"} style={{position: 'relative', margin: 0}}/>
         </div>
-        <div style={{flexDirection:'row', display: 'flex'}}>
-          <img src={`${process.env.PUBLIC_URL}/screen5.png`} width={"25%"} style={{position: 'relative', margin: 0, marginLeft: '6%', marginRight: '6%'}}/>
+        <div style={{flexDirection:'row', display: 'flex', marginBottom: 50}}>
+          <img src={`${process.env.PUBLIC_URL}/screen5.png`} width={"25%"} style={{position: 'relative', margin: 0}}/>
+          <view style={{width: 20}}/>
           <FunctionText title={"4. 자기소개서"} contents={`AI 관상 이미지, AI 관상 분석 외에도 <span style='color: ${point_color};'>관심사 카테고리</span>를 선택하거나 <span style='color: ${point_color};'>글과 이미지</span>를 첨부하여 자신에 대해 소개할 수 있습니다. <br><br>상대방의 자기소개서를 읽고 마음에 든다면 하트를 보내 관심을 표현할 수 있습니다.`}/>
         </div>
         <div style={{flexDirection:'row', display: 'flex'}}>
           <FunctionText title={"5. 채팅"} contents={`하트를 수락한다면 채팅을 나눌 수 있습니다. 채팅을 나누며 친밀도가 증가할수록 <span style='color: ${point_color};'>서로의 관상 이미지는 실제 얼굴 이미지에 근접해집니다</span>.`}/>
-          <img src={`${process.env.PUBLIC_URL}/screen6.png`} width={"25%"} style={{position: 'relative', margin: 0, marginLeft: '6%', marginRight: '6%'}}/>
+          <view style={{width: 20}}/>
+          <img src={`${process.env.PUBLIC_URL}/screen6.png`} width={"25%"} style={{position: 'relative', margin: 0}}/>
         </div>
       </div>
 
       <text id={classes.contentTitle} style={{display: 'block'}}>3. 서비스 구조</text>
-      <img src={`${process.env.PUBLIC_URL}/system_architecture.png`} width={"100%"} style={{position: 'relative', margin: 0, backgroundColor: '#FFFFFF60', marginBottom: 90}}/>
+      <img src={`${process.env.PUBLIC_URL}/system_architecture.png`} width={"100%"} style={{position: 'relative', margin: 0, padding:20, paddingRight: 0, backgroundColor: '#FFFFFF60',marginBottom: 90}}/>
 
       <text id={classes.contentTitle} style={{display: 'block'}}>4. 사용법</text>
-      <div style={{columnCount: 2, paddingLeft: '25%', paddingRight: '25%', marginBottom: 90}}>
+      <div style={{columnCount: 2, paddingLeft: '20%', paddingRight: '20%', marginBottom: 90}}>
         <div style={{width: '100%', textAlign: 'center', paddingBottom: 50}}>
           <text id={classes.contentSubTitle} style={{display: 'block', marginBottom: 20}}>회원가입 및 로그인</text>
           <img src={`${process.env.PUBLIC_URL}/demo1.gif`} width={"80%"} style={{position: 'relative', margin: 0}}/>
         </div>
         <div style={{width: '100%', textAlign: 'center', paddingBottom: 50}}>
           <text id={classes.contentSubTitle} style={{display: 'block', marginBottom: 20}}>AI 이미지 생성</text>
-          <img src={`${process.env.PUBLIC_URL}/demo2.gif`} width={"80%"} style={{position: 'relative', margin: 0}}/>
+          <img src={`${process.env.PUBLIC_URL}/demo3.gif`} width={"80%"} style={{position: 'relative', margin: 0}}/>
         </div>
         <div style={{width: '100%', textAlign: 'center'}}>
           <text id={classes.contentSubTitle} style={{display: 'block', marginBottom: 20}}>자기소개서</text>
-          <img src={`${process.env.PUBLIC_URL}/demo3.gif`} width={"80%"} style={{position: 'relative', margin: 0}}/>
+          <img src={`${process.env.PUBLIC_URL}/demo5.gif`} width={"80%"} style={{position: 'relative', margin: 0}}/>
         </div>
         <div style={{width: '100%', textAlign: 'center', paddingBottom: 50}}>
           <text id={classes.contentSubTitle} style={{display: 'block', marginBottom: 20}}>기본정보</text>
-          <img src={`${process.env.PUBLIC_URL}/demo4.gif`} width={"80%"} style={{position: 'relative', margin: 0}}/>
+          <img src={`${process.env.PUBLIC_URL}/demo2.gif`} width={"80%"} style={{position: 'relative', margin: 0}}/>
         </div>
         <div style={{width: '100%', textAlign: 'center', paddingBottom: 50}}>
           <text id={classes.contentSubTitle} style={{display: 'block', marginBottom: 20}}>AI 관상 분석</text>
-          <img src={`${process.env.PUBLIC_URL}/demo5.gif`} width={"80%"} style={{position: 'relative', margin: 0}}/>
+          <img src={`${process.env.PUBLIC_URL}/demo4.gif`} width={"80%"} style={{position: 'relative', margin: 0}}/>
         </div>
         <div style={{width: '100%', textAlign: 'center'}}>
           <text id={classes.contentSubTitle} style={{display: 'block', marginBottom: 20}}>채팅</text>
@@ -109,7 +114,7 @@ function Contents({onHeightChange}) {
         <TeamMember image={`${process.env.PUBLIC_URL}/profile6.png`} name={"김찬진 (BE, 팀장)"} contents={`인증/인가, AI 관상 이미지, AI 관상 분석 연동<br>기본 정보, AI 관상 이미지, AI 관상 분석, 자기소개서<br>AI 관상 궁합 추천, 채팅방 내 친밀도에 따른 AI 관상 이미지 변화, AWS S3`}/>
       </div>
 
-      <text id={classes.contentTitle} style={{display: 'block'}}>6. 문서</text>
+      <text id={classes.contentTitle} style={{display: 'block'}}>6. 문서(포스터, 발표 ppt)</text>
       <div style={{display: 'inline-block', backgroundColor: '#FF847D', padding: 10, borderRadius: 10}}>
         <a id={classes.subtitle} href='https://drive.google.com/drive/u/0/folders/15CNdGg8UGcZfr9XU2z3XRlWpkxXgrowY' style={{textDecoration: 'none',color: '#fff'}}>
           구글 드라이브 링크
--- capstone-2024-18/src/Components/Desktop/FunctionText.jsx
+++ capstone-2024-18/src/Components/Desktop/FunctionText.jsx
@@ -2,12 +2,12 @@ import classes from './DesktopPage.module.css';
 
 function FunctionText({title, contents}) {
   return (
-    <div style={{flex: 1, marginTop: 50}}>
-      <div style={{marginLeft: 20, marginRight: 20, display: 'flex', flexDirection: 'column', minHeight: '60%'}}>
+    <div style={{flex: 1, marginTop: 20}}>
+      <div style={{display: 'flex', flexDirection: 'column', minHeight: '60%'}}>
         <div style={{width: 'fit-content', marginLeft: 20, paddingTop: 10, paddingBottom: 10, paddingLeft: 20, paddingRight: 20, backgroundColor: '#FF847D', borderTopLeftRadius: 10, borderTopRightRadius: 10}}>
           <text id={classes.subtitle} style={{fontWeight: 'bold'}}>{title}</text>
         </div>
-        <div style={{borderRadius: 20, background: '#FFFFFF60', padding: 40, flex: 1}}>
+        <div style={{borderRadius: 20, background: '#FFFFFF60', padding: 30, flex: 1, alignContent: 'center'}}>
           <div id={classes.contentSubTitle} dangerouslySetInnerHTML={{__html: contents}}/>
         </div>
       </div>
